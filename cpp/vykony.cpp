8492d08b03d4c67e2ec7c117935dc093c470f151
Tomas Bahnik <tomas.bahnik@aevi.cz>
2018-12-10 14:38:23 +0100

    ciny a jalovy vykon

--- cpp/vykony.cpp
+++ cpp/vykony.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+/*
+ * Provovadi vypocet parametru v obvodu realne civky se 
+ * zdrojem stridaveho napeti.
+ * 
+ * Na zaklade zadanych hodnot napeti, proudu, frekvence a 
+ * vnitrniho odporu civky urci fazovy posuv mezi napetim a proudem
+ * a cinny, jalovy a zdanlivy vykon
+ */
 
 int main(int argc, char** argv) 
 { 
@@ -25,7 +33,19 @@ int main(int argc, char** argv)
 	cout<<"impedance="<<impedance<<" ohm (napeti/proud) \n";
 	cout<<"indukcnost="<<induktance/(2*M_PI*frekvence)<<" H\n";
 	cout<<"tg fi="<<induktance/odpor<<"\n";
-	cout<<"fi="<<atan(induktance/odpor)<<" rad\n";
-	cout<<"fi="<<atan(induktance/odpor)*(180/M_PI)<<" stupnu\n";
+	
+	// fazovy posun v radianech 
+	double fi_rad = atan(induktance/odpor);
+	cout<<"fi="<<fi_rad<<" rad\n";
+	// fazovy posun ve stupnich
+	cout<<"fi="<<fi_rad*(180/M_PI)<<" stupnu\n";
+	
+	double cinny_vykon = napeti*proud*cos(fi_rad);
+	double jalovy_vykon = napeti*proud*sin(fi_rad);
+	
+	printf ("cinny vykon = %fW, jalovy vykon = %fW\n\n", cinny_vykon, jalovy_vykon);
+	printf("Kontrola vypoctu cinneho a jaloveho vykonu\n");
+	printf ("sqrt(cinny_vykon^2 + jalovy_vykon^2) = %fW, zdanlivy vykon %fW\n", sqrt(pow(cinny_vykon,2) + pow(jalovy_vykon,2)), napeti*proud);
+	
 	return 0;
 }
