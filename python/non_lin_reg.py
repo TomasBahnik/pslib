b0e6fd10ccdd6dcbd4648beeaeb9e6f85701aab7
Tomas Bahnik <tbahnik@casenetllc.com>
2019-02-04 09:11:04 +0100

    skutecne hodnoty

--- python/non_lin_reg.py
+++ python/non_lin_reg.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 mpl.rcParams["axes.spines.right"] = False
 mpl.rcParams["axes.spines.top"] = False
 # xm = np.array([0,1,2,3,4,5])
-xm = np.array([1, 2, 3, 4, 5, 6, 7.5, 12.4, 13])
-ym = np.array([10, 25, 35, 43, 47, 52, 54, 55, 55.5])
+# U	 (V)	2,	4,	6,	8,	10,	12,	14,	16,	18,	20,	22
+xm = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22])
+# I (mA)	90.4,	143.1,	175.7,	199,	215,	228,	239,	250,	259,	268,	276
+ym = np.array([90.4, 143.1, 175.7, 199, 215, 228, 239, 250, 259, 268, 276])
 
 #### Solution
 m = GEKKO()
@@ -47,8 +49,10 @@ p5 = [c[5].value[0], c[4].value[0], c[3].value[0],
       c[2].value[0], c[1].value[0], c[0].value[0]]
 
 # plot fit
-plt.plot(xm, ym, 'ro', markersize=5)
-xp = np.linspace(0, xm.max(), 50)
+plt.plot(xm, ym, 'bo', markersize=4)
+print('xm max: {}'.format(xm.max()))
+print('xm min: {}'.format(xm.min()))
+xp = np.linspace(xm.min(), xm.max(), 50)
 y2 = 34.6 - 6.23 * xp
 plt.plot(xp, y2)
 plt.plot((0, 6.5), (30, 30), 'k:')
@@ -56,12 +60,12 @@ plt.plot((6.5, 6.5), (30, 0), 'k:')
 # plt.plot(xp, np.polyval(p1, xp), 'b--', linewidth=2)
 # plt.plot(xp, np.polyval(p2, xp), 'k--', linewidth=3)
 # plt.plot(xp, np.polyval(p3, xp), 'g--', linewidth=2)
-plt.plot(xp, np.polyval(p5, xp), 'k:', linewidth=2)
+plt.plot(xp, np.polyval(p5, xp), 'k:', linewidth=1)
 # plt.legend(['Data', 'Linear', 'Quadratic', 'Cubic'], loc='best')
 # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.html
 # plt.legend(['Data', '3rd', '5th'], loc='best')
-plt.xlabel('Napětí[V]')
-plt.ylabel('Proud[A]')
+plt.xlabel('U [V]')
+plt.ylabel('I [mA]')
 plt.grid(True)
 plt.axis([0, 1.1 * np.max(xm), 0, 1.1 * np.max(ym)])
 plt.minorticks_on()
