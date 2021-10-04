b85d81e6f2fcc7b0d2ff7bd8651c5d015336f4e5
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-10-04 21:03:07 +0200

    refactor , comment

--- python/t01/seq_sum.py
+++ python/t01/seq_sum.py
@@ -3,7 +3,7 @@ import sys
 
 nums = [149, 439, 457, 509, 521, -109, -101, 20, -7, -7, -2, 0, 1, 3, 5, 5, 10, 23, 37, 271, 4, 3, 11, 541, 67, 79,
         83, 433, 227, 137, 149, 439, 457, 509]
-
+# first 100 primes
 PRIMES_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
               107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
               227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347,
@@ -11,17 +11,18 @@ PRIMES_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61
               467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
 
 
-def is_prime_100(x):
+def is_num_prime(x):
+    """ Decides if the abs(x) is prime number basec on presence in first 100 primes. """
     if abs(x) in PRIMES_100:
         return True
     else:
         return False
 
 
-def seq_is_interrupted(current, previous):
+def is_seq_interrupted(current, previous):
     if previous is None:
         return True
-    d = is_prime_100(previous) and not is_prime_100(current)
+    d = is_num_prime(previous) and not is_num_prime(current)
     if d:  # not prime
         return True
     if current < previous:  # decreasing
@@ -34,19 +35,21 @@ def add_seq(ss, s):
         ss += [s]
 
 
-seq = [] # empty array/list
+seq = []  # empty array/list
 seqs = []
-seq += [nums[0]] # start at leftmost element => for cycle below starts at 1 not 0
+seq += [nums[0]]  # start at leftmost element => for cycle below starts at 1 not 0
 # find longest non-decreasing AND not interrupted sequence
 for i in range(1, len(nums)):
+    # p (previous) is None for empty sequence
     p = seq[-1] if len(seq) > 0 else None
     c = nums[i]
-    if seq_is_interrupted(c, p):
+    if is_seq_interrupted(c, p):
         add_seq(seqs, seq)
         seq = []
-        if is_prime_100(c):
+        if is_num_prime(c):
             seq += [c]
-    if p is not None and c >= p and is_prime_100(c):  # non-decreasing
+    # add to already started sequence non-decreasing prime
+    if p is not None and c >= p and is_num_prime(c):
         seq += [c]
 add_seq(seqs, seq)  # add last seq
 print(seqs)
