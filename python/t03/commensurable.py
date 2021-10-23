934ef0bda7db119a7316ec78deb39e457998dca9
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-10-23 23:05:35 +0200

    wrap test into function

--- python/t03/commensurable.py
+++ python/t03/commensurable.py
@@ -21,9 +21,9 @@ def prepare_table(n, m):
     if n < 2 or m < 2 or n == m:
         return 'ERROR'
     table = []
-    for i in range(n, m + 1):  # row
-        row = []
-        for j in range(n, m + 1):  # column
+    for i in range(n, m + 1):  # row <n,m> i.e m inclusive
+        row = []  # new row
+        for j in range(n, m + 1):  # add letter to table[i,j] position according to type
             if soudelna(i, j):
                 row += [soudelna_symbol]
             elif prime(i) or prime(j):
@@ -38,24 +38,19 @@ def print_table(table):
     if type(table) is str:
         print(table)
     elif type(table) is list:
-        i = 1
         for row in table:
-            print('{}. {}'.format(i, row))
-            i += 1
+            print(row)
     else:
         print('Unknown type {}'.format(type(table)))
 
 
+def test(vstup):
+    print('\nInput {}'.format(vstup))
+    print_table(prepare_table(min(vstup), max(vstup)))
+
+
 if __name__ == '__main__':
-    vstup = [2, 10]
-    print(vstup)
-    print_table(prepare_table(min(vstup), max(vstup)))
-
-    vstup = [11, 10]
-    print(vstup)
-    print_table(prepare_table(min(vstup), max(vstup)))
-
-    vstup = [2, 1]
-    print(vstup)
-    print_table(prepare_table(min(vstup), max(vstup)))
+    test([2, 10])
+    test([11, 10])
+    test([2, 1])
     sys.exit(0)
