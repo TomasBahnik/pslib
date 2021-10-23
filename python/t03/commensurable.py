0e8bf28166179a651f1493dcf5bddb57f72b94b0
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-10-23 22:37:18 +0200

    print tabular output

--- python/t03/commensurable.py
+++ python/t03/commensurable.py
@@ -3,23 +3,43 @@
 # table  n x m : min(a,b) <= m,n, <= max(a,b)
 import sys
 
-soudelna = 'x'
-prvocisla = 'p'
+from common.functions import gcd_euclid, prime
+
+soudelna_symbol = 'x'
+prvocisla_symbol = 'p'
 mezera = ' '
 separator = '|'
 
 
+def soudelna(a, b):
+    if gcd_euclid(a, b) > 1:
+        return True
+    return False
+
+
 def prepare_table(n, m):
     table = []
-    for i in range(0, n):  # row
+    for i in range(n, m + 1):  # row
         row = []
-        for j in range(0, m):  # column
-            row += [i, j]
+        for j in range(n, m + 1):  # column
+            if soudelna(i, j):
+                row += [soudelna_symbol]
+            elif prime(i) or prime(j):
+                row += [prvocisla_symbol]
+            else:
+                row += [mezera]
         table += [row]
     return table
 
 
+def print_table(table):
+    for row in table:
+        print(row)
+
+
 if __name__ == '__main__':
-    # n x m table
-    print(prepare_table(2, 4))
+    i = 1
+    for row in prepare_table(2, 10):
+        print('{}. {}'.format(i, row))
+        i += 1
     sys.exit(0)
