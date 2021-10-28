3b5d7eaa13a19d7d95351ae9919702419a3ee69c
pantom <pantom@centrum.cz>
2021-10-28 20:04:53 +0200

    puleni intervalu s float input

--- python/cv02/root.py
+++ python/cv02/root.py
@@ -8,20 +8,20 @@ def f(x):
     return y
 
 
-# a5 = float(input())
-# a4 = float(input())
-# a3 = float(input())
-# a2 = float(input())
-# a1 = float(input())
-# a0 = float(input())
+a5 = float(input())
+a4 = float(input())
+a3 = float(input())
+a2 = float(input())
+a1 = float(input())
+a0 = float(input())
 
 # # root = 100 (upper bound - zadna iterace)
-a0 = -700
-a1 = 8
-a2 = 0
-a3 = 0
-a4 = 0
-a5 = 0
+# a0 = -700
+# a1 = 8
+# a2 = 0
+# a3 = 0
+# a4 = 0
+# a5 = 0
 
 # 3 odmocnina a3 * x ** 3 + a0 = 0
 # x**3 - 27 = 0
@@ -42,14 +42,14 @@ step = 0
 # TODO muzu pouzit abs ??
 while abs(x1 - x2) >= eps:
     xp = (x1 + x2) / 2
-    # step += 1 # step = step + 1
-    # acc = abs(x1 - x2)
-    # print("{} : {}, eps {}".format(step, acc, eps))
+    step += 1 # step = step + 1
+    acc = abs(x1 - x2)
+    print("{} : {}, eps {}".format(step, acc, eps))
     if f(xp) < 0:
         x1 = xp
     else:  # tady uz je f(xp) >= 0
         x2 = xp
 
-# print("f({}) = {}".format(xp, f(xp)))
+print("f({}) = {}".format(xp, f(xp)))
 print(xp)
 sys.exit(0)
