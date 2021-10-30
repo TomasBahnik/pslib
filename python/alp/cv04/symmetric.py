793acc2430463469a7b6fb6b8885e2da92c48980
pantom <pantom@centrum.cz>
2021-10-30 21:08:19 +0200

    funkcni verze symmetric.py

--- python/alp/cv04/symmetric.py
+++ python/alp/cv04/symmetric.py
@@ -1,69 +1,40 @@
-# from alp.common.funkce import sym
-# from alp.common.funkce import poslup
-
-def sym(x):
-    """nalezne symetrickou posloupnost"""
-    if len(x) == 0:
+def is_seq_symmetric(sequence):
+    if len(sequence) == 0:  # empty list is not symmetric
         return False
-    for i in range(0, len(x) // 2):
-        if x[i] != x[- (1 + i)]:
-            # print("{} != {}".format(i, -(i + 1)))
-            # print("{} != {}".format(pole[i], pole[-(i + 1)]))
+    for i in range(1, len(sequence) // 2 + 1):
+        # print("{} != {} : {}".format(current[i - 1], current[-i], current[i - 1] != current[-i]))
+        if sequence[i - 1] != sequence[-i]:
             return False
-        # print("index {} = {}".format(i, -(i + 1)))
-        # print("hodnota {} = {}".format(pole[i], pole[-(i + 1)]))
     return True
 
 
-def ind_delk(z):
-    delka = 1
-    if sym(z):
-        for p in range(0, len(z) + 1):
-            n = 0
-            if p > n:
-                n = p
-                if delka < n:
-                    delka = n
-                    if p != len(z):
-                        return False
-        return True
-
-
-def posloup(pole):
-    delka = 1
-    for i in range(0, len(pole) + 1):
-        l_p = len(pole)
-        if l_p == 0:
+def symmetric_sub_seq(sequence):
+    seq_length = 0
+    seq_sum = -1
+    seq_idx = -1
+    for i in range(0, len(sequence) + 1):
+        l_s = len(sequence)  # sequence is modified by poping last item at the end
+        if l_s == 0:  # when sequence is empty print the last values = output
+            print(seq_idx, seq_length)  # tisk navratove hodnoty NEMAZAT
             return
-        for j in range(i, l_p):
-            pod_pole = pole[j:l_p]
-            if sym(pod_pole):
-                if ind_delk(pod_pole):
-                    print(j, delka)
+        for j in range(0, l_s):
+            sub_seq = sequence[j:l_s]
+            # print("{}:{} sub sequence {}".format(j, l_s, sub_seq))
+            if is_seq_symmetric(sub_seq):
+                # print("symmetric sub sequence : {}".format(sub_seq))
+                cur_length = len(sub_seq)
+                cur_sum = sum(sub_seq)
+                if cur_length > seq_length:
+                    seq_idx = j
+                    # print("length increased {} -> {} index = {}".format(seq_length, cur_length, seq_idx))
+                    seq_length = cur_length
+                    seq_sum = cur_sum
+                if cur_length == seq_length and cur_sum > seq_sum:
+                    seq_idx = j
+                    # print("sum increased {} -> {}. index = {}".format(seq_sum, cur_sum, seq_idx))
+                    seq_sum = cur_sum
+        sequence.pop()
 
-        pole.pop()
 
-
-# pro načtení a převedení vstupu na pole celých čísel můžete použít příkaz
-# https://cw.fel.cvut.cz/wiki/courses/b3b33alp/cviceni/t01
-# s3 = list(map(int, input().split()))
-s3 = [-14, -8, -9, 2, -18, 12, 1, -1, -14, -14, 13, -2, 15]
-# s3 = [1, 14, 2, 14, 4]
-posloup(s3)
-# def test_sym():
-#     x = []
-#     print("vstup {}: je symetricka {}".format(x, sym(x)))
-#     x = [1, 2, 3, 5, 9, 89, 45, 85, 5, 105, 68, 1]
-#     print("vstup {}: je symetricka {}".format(x, sym(x)))
-#     s1 = [10, -1, 7, 78, 53, 78, 7, -1, 10]
-#     print("vstup {}: je symetricka {}".format(s1, sym(s1)))
-#     s2 = [12, -16, -7, -18, -5, -3, 2, 8, 9, -14, -18, -9, 11, -7, -3, 4, -10, 4, -3, -7, 11, -12, -14, 5, -11, -7, 7,
-#           13, 2, 19, 12, 11]
-#     s3 = [-14, -8, -9, 2, -18, 12, 1, -1, -14, -14, 13, -2, 15]
-#     s4 = [-4, -12, 17, 18, -8, 7]
-#     s5 = [2, 2, 2, 2, 2, 2, 2, 2]
-#     s6 = [10, 8, 2, 5, 8, 10]
-#     print("vstup {}: je symetricka {}".format(s6, sym(s6)))
-
-# print('\ninput={}, length={}'.format(s2, len(s2)))
-# symmetric_sub_seq(s2)
+pole = list(map(int, input().split()))
+symmetric_sub_seq(pole)
