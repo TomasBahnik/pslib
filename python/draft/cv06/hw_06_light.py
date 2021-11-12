57165919217a095349f34c785a74fb73a1fdf54b
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-11-12 09:58:52 +0100

    add comments and TODOs

--- python/draft/cv06/hw_06_light.py
+++ python/draft/cv06/hw_06_light.py
@@ -13,6 +13,17 @@ Písmena, která zbudou po vynechání všech písmen vyskytujících se v zadan
 Písmena tiskněte zleva doprava odshora dolů
 
 osmismerka.txt + slova.txt => odfoukali
+
+Overit : Vynechat by se meli NE VSECHNA pismena V CELE MATICI co se vyskytnout v nalezenych slovech, ale jen
+jenom ta v mistech, kde bylo nalezene slovo - ale po nalezeni slova je nutne je ponechat v matici aby zustaly slova
+co se mohou krizit - tj. udelat v miste pismena jen 'pruhlednou' znacku a teprve nakonci je mozne takto oznacene
+misto smazat resp. preskocit
+
+Je celkem 4x2 = 8 moznych smeru (v dane uloze prvni 3 bez reverse)
+   * left -> right  + reverse
+   * top -> down + reverse
+   * left-top -> right-bottom + reverse
+   * left-bottom -> right-top + reverse
 """
 
 import sys
@@ -22,23 +33,32 @@ from draft.shared.matrices import load_char_matrix, diagonals, column
 found_words = []
 
 
+# TODO search for substrings in given directions
+#  and for substring start index end index or length
+#  for diagonals and columns ensure the search in right direction
+#  left -> right, top -> down, left-top -> right-bottom
+def find_word(search_in, word):
+    # TODO no need to use list for rows, only for columns and diagonals
+    s1 = ''.join(search_in)
+    s2 = ''.join(word)
+    if search_in == word:
+        print("word {} found".format(word))
+        found_words.append(word)
+
+
 def test_rows_columns(matrix, words):
     print("rows ...")
     for word in words:
         l = len(column(matrix, 0))
         for idx in range(0, l):
             search_in = matrix[idx]
-            if search_in == word:
-                print("word {} found".format(word))
-                found_words.append(word)
+            find_word(search_in, word)
     print("columns ...")
     for word in words:
         l = len(matrix[0])
         for idx in range(0, l):
             search_in = column(matrix, idx)
-            if search_in == word:
-                print("word {} found".format(word))
-                found_words.append(word)
+            find_word(search_in, word)
 
 
 def test_diagonals(matrix, words):
@@ -52,9 +72,7 @@ def test_diagonals(matrix, words):
         details = diagonals(matrix, diff, down=False)
         search_in = details[0]  # diagonal sequence
         for word in words:
-            if search_in == word:
-                print("word {} found".format(word))
-                found_words.append(word)
+            find_word(search_in, word)
 
 
 def empty_matrix(rows, columns):
@@ -63,10 +81,19 @@ def empty_matrix(rows, columns):
     return ret
 
 
+def remove_words(m, f_w):
+    for word in f_w:
+        print("\n*** {}".format(word))
+        for i in range(0, len(m)):
+            m[i] = ([char for char in m[i] if char not in word])
+    return m
+
+
 if __name__ == '__main__':
     file_with_matrix = sys.argv[1]
     file_with_words = sys.argv[2]
     matrix = load_char_matrix(file_with_matrix)
+    # TODO keep words as list of strings
     words = load_char_matrix(file_with_words)
     matrix_columns = len(matrix[0])
     matrix_rows = len(column(matrix, 0))
@@ -74,8 +101,6 @@ if __name__ == '__main__':
     test_rows_columns(matrix, words)
     test_diagonals(matrix, words)
     # print(found_words)
-    for word in found_words:
-        print("\n*** {}".format(word))
-        for row in matrix:
-            print([char for char in row if char not in word])
+    tmp = remove_words(matrix, found_words)
+    print("len {}, tmp {}".format(len(tmp), tmp))
     sys.exit(0)
