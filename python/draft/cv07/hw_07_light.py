d7798f4c5cbda595d1f7db36e3303cf4334942ce
Tomas Bahnik <pantom@centrum.cz>
2021-11-17 00:15:47 +0100

    hw_07_light.py prvni verze

--- python/draft/cv07/hw_07_light.py
+++ python/draft/cv07/hw_07_light.py
@@ -1,30 +1,37 @@
-n = int(input())
+# n = int(input())
+import sys
+
 x = float(input())
 
-def recurrent(a, b):
-    for i in range(0, n+1):
+hint = []
 
 
+def r0():
+    return -1
 
 
+def r1(y):
+    return y
 
 
+def r2(y):
+    return -y ** 2
 
 
+def init_hint(y):
+    hint.append(r0())
+    hint.append(r1(y))
+    hint.append(r2(y))
 
 
+def r3(y):
+    return 1 / 3 * r2(y) + (-1) ** 3 * r1(y) + 2 / y * r0()
 
 
+if x == 0:
+    print("error x nemuze byt".format(x))
+    sys.exit(1)
 
-
-
-
-
-
-
-
-
-
-
-
-sys exit()
+init_hint(x)
+print(hint)
+print(r3(x))
