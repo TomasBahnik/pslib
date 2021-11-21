3ca64bf4f36bbc247dd4affdbce507365a9596d8
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-11-21 14:25:15 +0100

    add stone cell reindex and stone boarder

--- python/draft/cv08/hw_08_light.py
+++ python/draft/cv08/hw_08_light.py
@@ -6,6 +6,16 @@
 # pokud existuje více řešení, vypište libovolné z nich
 import sys
 
+from draft.shared.general import debug_print
+
+DEBUG_PRINTS = True
+# indexes
+STONE_COLOR = 0
+STONE_CELLS = 1
+STONE_BORDER = 2  # used for sort
+CELL_ROW = 0
+CELL_COLUMN = 1
+
 
 def read_stones(filename):
     stones = []  # vystup funkce - seznam nactenych kamenu
@@ -25,31 +35,54 @@ def read_stones(filename):
 
 
 # see 1 and 2
+# all stones must be used and fill the whole matrix
 def check_stone_areas(rows, cols, stones):
     area = 0
     for stone in stones:
-        s_a = len(stone[1])
-        area += s_a
-        print("color={}, area ={}".format(stone[0], s_a))
+        s_c = len(stone[STONE_CELLS])
+        area += s_c
+        print("color={}, area ={}".format(stone[0], s_c))
     if area != rows * cols:
         print('NOSOLUTION')
         return -1
     return area
 
 
-filename = sys.argv[1]
+# relabel stone cells as if it is at
+# top left corner, shift all cells so that
+# min row = 0 and min column = 0
+def move_to_top_left_corner(stone):
+    stone_cells = stone[STONE_CELLS]
+    min_row = min([cell[CELL_ROW] for cell in stone_cells])
+    min_col = min([cell[CELL_COLUMN] for cell in stone_cells])
+    for cell in stone_cells:
+        cell[CELL_ROW] -= min_row
+        cell[CELL_COLUMN] -= min_col
+    return stone_cells
 
-M, N, stones = read_stones(filename)
-print("matrix {}x{}".format(M, N))
-check_stone_areas(M, N, stones)
 
-# n-ty kamen je stones[n]
-# jeho barva je stones[n][0], jeho bunky jsou stones[n][1]
+def stone_border(stone):
+    n = 0
+    cells = stone[STONE_CELLS]
+    for cell in cells:
+        l = [cell[CELL_ROW] - 1, cell[CELL_COLUMN]] in cells
+        r = [cell[CELL_ROW] + 1, cell[CELL_COLUMN]] in cells
+        b = [cell[CELL_ROW], cell[CELL_COLUMN] + 1] in cells
+        t = [cell[CELL_ROW], cell[CELL_COLUMN] - 1] in cells
+        n += len([x for x in [l, r, b, t] if x is False])
+    stone.append(n)
+    return n
 
-# vypis pozic n-teho kamene:
-n = 5  # napriklad chceme 0.kamen
-print("color={}".format(stones[n][0]))
 
-for cellindex in range(len(stones[n][1])):
-    row, col = stones[n][1][cellindex]
-    print(row, col)
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    M, N, stones = read_stones(filename)
+    for stone in stones:
+        # debug_print("Before {}]".format(stone), DEBUG_PRINTS)
+        moved_cells = move_to_top_left_corner(stone)
+        boarder = stone_border(stone)
+        debug_print("Stone {} : cells={}, border={}]"
+                    .format(stone[STONE_COLOR], stone[STONE_CELLS], stone[STONE_BORDER]), DEBUG_PRINTS)
+
+    print("matrix {}x{}".format(M, N))
+    check_stone_areas(M, N, stones)
---
+++ python/draft/shared/general.py
@@ -0,0 +1,3 @@
+def debug_print(message, print_debug):
+    if print_debug:
+        print(message)
