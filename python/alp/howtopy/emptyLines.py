065c6b49fe1e2877a34b8e82c70d900e38cb763d
Tomas Bahnik <pantom@centrum.cz>
2021-12-05 17:36:03 +0100

    howtopy_functions

---
+++ python/alp/howtopy/emptyLines.py
@@ -0,0 +1,61 @@
+""""
+def addline():
+    print()
+
+
+def compare(x, y):
+    if x > y:
+        print(1)
+    elif x == y:
+        print(0)
+    else:
+        print(-1)
+
+
+compare(5, 4)
+addline()
+compare(7, 7)
+addline()
+compare(2, 3)
+
+"""
+
+"""
+def hypotenus(x, y):
+    p = (x ** 2 + y ** 2) ** (1 / 2)
+    print(p)
+
+
+hypotenus(3, 4)
+hypotenus(12, 5)
+hypotenus(7, 24)
+"""
+
+import math
+
+# def slope(x1, y1, x2, y2):
+
+# slope(5, 3, 4, 2)
+
+"""
+def is_multiple(m, n):
+    if m % n == 0:
+        print(True)
+    else:
+        print(False)
+
+is_multiple(12, 3)
+is_multiple(12, 4)
+is_multiple(12, 5)
+
+"""
+
+
+# def is_divisible(a, b):
+#     if (c, int)
+#         a / b = c
+#
+#
+# is_divisible(20, 4)
+# is_divisible(21, 8.5)
+
