a74299adea20eba30e86dd353eff8fe6df602d43
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-12-05 16:59:13 +0100

    refactor linalg.py

--- python/analysis/linalg.py
+++ python/analysis/linalg.py
@@ -6,20 +6,24 @@ x, y, z, t = symbols('x y z t')
 k, m, n = symbols('k m n', integer=True)
 
 
-def factor_poly():
-    return factor(x ** 4 - 6 * x ** 2 + 8 * x - 3)
+def cv9():
+    cv9_matrix = Matrix([[x, 1, 1, 1], [1, x, 1, 1], [1, 1, x, 1], [1, 1, 1, x]])
+    det_cv09 = cv9_matrix.det()
+    f_p = factor(det_cv09)
+    print(latex(cv9_matrix))
+    print(latex(det_cv09))
+    print(latex(f_p))
+
+
+def matrices():
+    m_a = Matrix([[34, 22, 19, 12], [72, 87, 162, 122], [69, 69, 420, 89]])
+    print(latex(m_a))
+    m_a_transposed = m_a.T
+    print(latex(m_a_transposed))
 
 
 if __name__ == '__main__':
     init_printing()  # doctest: +SKIP
-    A = Matrix([[34, 22, 19, 12], [72, 87, 162, 122], [69, 69, 420, 89]])
-    print(latex(A))
-    AT = A.T
-    print(latex(AT))
-    cv9_matrix = Matrix([[x, 1, 1, 1], [1, x, 1, 1], [1, 1, x, 1], [1, 1, 1, x]])
-    print(latex(cv9_matrix))
-    det_cv09 = cv9_matrix.det()
-    print(latex(det_cv09))
-    print(latex(factor_poly()))
-
+    # matrices()
+    cv9()
     sys.exit(0)
