ee701d7cf31dd926cdd1ed6220258b50c4c0c99b
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-12-06 13:07:31 +0100

    obecna funckce pro vsechny ulohy

--- python/analysis/linalg.py
+++ python/analysis/linalg.py
@@ -2,6 +2,8 @@ import sys
 
 from sympy import *
 
+latex_new_line = "\\\\"
+
 x, y, z, t, a = symbols('x y z t a')
 p_1, p_2, p_3, p_4, p_5 = symbols('p_1 p_2 p_3 p_4 p_5')
 k, m, n = symbols('k m n', integer=True)
@@ -13,32 +15,38 @@ def lin_equations():
     linsolve((A, b), [p_1, p_2, p_3])
 
 
-def p812():
-    matrix = Matrix([[1, 1, 1, 1, 0], [1, -1, -1, 0, 0], [1, 1, -1, -1, -1], [2, 2, 0, 0, -1], [1, 1, 5, 5, 2]])
-    b = Matrix([3, 4, 5, 8, -1])
+def matrix_properties(matrix, b, p):
     det_a = matrix.det()
-    particular = linsolve((matrix, b), [p_1, p_2, p_3, p_4, p_5])
-    print("\\mA={} \\\\".format(latex(matrix)))
-    print("det(A)={}={} \\\\".format(latex(det_a), latex(factor(det_a))))
-    print("p={}".format(latex(particular)))
-    # kernel
+    rank = matrix.rank()
+    ker = matrix.nullspace()
+    particular = linsolve((matrix, b), p)
+    print("\\mA={} {}".format(latex(matrix), latex_new_line))
+    print("det(A)={}={} {}".format(latex(det_a), latex(factor(det_a)), latex_new_line))
+    print("p={} {}".format(latex(particular), latex_new_line))
+    print("kernel={} {}".format(latex(ker), latex_new_line))
+    print("rank={} {}".format(rank, latex_new_line))
+    # homogenni rovnice
     b = Matrix([0, 0, 0, 0, 0])
-    kernel = linsolve((matrix, b), [p_1, p_2, p_3, p_4, p_5])
-    print("ker={}".format(latex(kernel)))
+    homog_sol = linsolve((matrix, b), p)
+    print("solution (b=0)={}".format(latex(homog_sol)))
+
+
+matrix_p812 = Matrix([[1, 1, 1, 1, 0], [1, -1, -1, 0, 0], [1, 1, -1, -1, -1], [2, 2, 0, 0, -1], [1, 1, 5, 5, 2]])
+b_p812 = Matrix([3, 4, 5, 8, -1])
+symbols_p812 = [p_1, p_2, p_3, p_4, p_5]
+
+
+def p812():
+    matrix_properties(matrix_p812, b_p812, symbols_p812)
+
+
+matrix_cv9 = Matrix([[a, 1, 1, 1], [1, a, 1, 1], [1, 1, a, 1], [1, 1, 1, a]])
+b_cv9 = Matrix([1, 1, 1, 1])
+symbols_cv9 = [p_1, p_2, p_3, p_4]
 
 
 def cv9():
-    matrix = Matrix([[a, 1, 1, 1], [1, a, 1, 1], [1, 1, a, 1], [1, 1, 1, a]])
-    b = Matrix([1, 1, 1, 1])
-    det_a = matrix.det()
-    particular = linsolve((matrix, b), [p_1, p_2, p_3, p_4])
-    print("\\mA={} \\\\".format(latex(matrix)))
-    print("det(A)={}={} \\\\".format(latex(det_a), latex(factor(det_a))))
-    print("p={}".format(latex(particular)))
-    # kernel
-    b = Matrix([0, 0, 0, 0])
-    kernel = linsolve((matrix, b), [p_1, p_2, p_3, p_4])
-    print("ker={}".format(latex(kernel)))
+    matrix_properties(matrix_cv9, b_cv9, symbols_cv9)
 
 
 def matrices():
