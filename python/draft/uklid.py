f8d377c03bf9d6536b415773cb17b90d7327a60e
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2021-12-30 15:32:07 +0100

    correctly split weeks, prints

--- python/draft/uklid.py
+++ python/draft/uklid.py
@@ -1,20 +1,45 @@
 import calendar
+import datetime
+
+YEAR = 2022
+
+
+def weekday_from_date_idx(day, month, year):
+    return datetime.date(day=day, month=month, year=year).weekday()
+
+
+def weekday_from_date(day, month, year):
+    return calendar.day_name[
+        datetime.date(day=day, month=month, year=year).weekday()
+    ]
+
+
+my_text_calendar = calendar.TextCalendar()
 
 # Show every month
 for month in range(1, 13):
 
     # Compute the dates for each week that overlaps the month
-    c = calendar.monthcalendar(2022, month)
-    first_week = c[0]
-    second_week = c[1]
-    third_week = c[2]
-
-    # If there is a Thursday in the first week, the second Thursday
-    # is in the second week.  Otherwise the second Thursday must
-    # be in the third week.
-    if first_week[calendar.THURSDAY]:
-        meeting_date = second_week[calendar.THURSDAY]
-    else:
-        meeting_date = third_week[calendar.THURSDAY]
-
-    print('%3s: %2s' % (month, meeting_date))
+    c = calendar.monthcalendar(YEAR, month)
+    # c = my_text_calendar.monthdayscalendar(YEAR, month)
+    for week in c:
+        if week[calendar.MONDAY] and week[calendar.SUNDAY]:  # full week
+            start_day = week[calendar.MONDAY]
+            start_idx = week.index(start_day)
+            start_day_name = calendar.day_name[start_idx]
+            end_day = week[calendar.SUNDAY]
+            end_idx = week.index(end_day)
+            end_day_name = calendar.day_name[end_idx]
+        else:
+            # first non-zero index
+            first_day_idx = week.index(next(filter(lambda x: x != 0, week)))
+            # first_day_idx = week.index(1)
+            start_day = week[first_day_idx]
+            start_day_name = calendar.day_name[first_day_idx]
+            last_day_idx = week.index(next(filter(lambda x: x == 0, week)))
+            end_day = week[last_day_idx - 1]
+            # end_idx = week.index(end)
+            end_day_name = calendar.day_name[last_day_idx - 1]
+            # print('week {}'.format(week))
+        print('week {} : {}.{}.{} ({}) - {}.{}.{} ({})'.
+              format(week, start_day, month, YEAR, start_day_name, end_day, month, YEAR, end_day_name))
