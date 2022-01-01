1d8f78c5a1acb76c94fbb50a3d921cf3cfa82890
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-01-01 19:47:14 +0100

    use `yeardatescalendar` and sorted set of weeks as datetime.date

--- python/draft/uklid.py
+++ python/draft/uklid.py
@@ -15,31 +15,13 @@ def weekday_from_date(day, month, year):
 
 
 my_text_calendar = calendar.TextCalendar()
+c = my_text_calendar.yeardatescalendar(YEAR)
+unique_weeks = set()
+for month in c:
+    for week in month:
+        for days in week:
+            my_week = (days[calendar.MONDAY], days[calendar.SUNDAY])
+            unique_weeks.add(my_week)
 
-# Show every month
-for month in range(1, 13):
-
-    # Compute the dates for each week that overlaps the month
-    # c = calendar.monthcalendar(YEAR, month)
-    c = my_text_calendar.monthdayscalendar(YEAR, month)
-    for week in c:
-        if week[calendar.MONDAY] and week[calendar.SUNDAY]:  # full week
-            start_day = week[calendar.MONDAY]
-            start_idx = week.index(start_day)
-            start_day_name = calendar.day_name[start_idx]
-            end_day = week[calendar.SUNDAY]
-            end_idx = week.index(end_day)
-            end_day_name = calendar.day_name[end_idx]
-        else:
-            # first non-zero index
-            first_day_idx = week.index(next(filter(lambda x: x != 0, week)))
-            # first_day_idx = week.index(1)
-            start_day = week[first_day_idx]
-            start_day_name = calendar.day_name[first_day_idx]
-            last_day_idx = week.index(next(filter(lambda x: x == 0, week)))
-            end_day = week[last_day_idx - 1]
-            # end_idx = week.index(end)
-            end_day_name = calendar.day_name[last_day_idx - 1]
-            # print('week {}'.format(week))
-        print('{} : {}.{}.{} ({}) - {}.{}.{} ({})'.
-              format(week, start_day, month, YEAR, start_day_name, end_day, month, YEAR, end_day_name))
+for w in sorted(unique_weeks):
+    print('{} - {}'.format(w[0].strftime("%d.%m.%Y"), w[1].strftime("%d.%m.%Y")))
