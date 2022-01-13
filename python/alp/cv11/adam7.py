d2f9b1a9b53a03043a4e5381634cf407cc96b6d3
pantom <pantom@centrum.cz>
2022-01-13 23:46:59 +0100

    adam7.py inputs

---
+++ python/alp/cv11/adam7.py
@@ -0,0 +1,14 @@
+c_r = list(map(int, input().strip().split()))
+values = list(map(int, input().strip().split()))
+"""sloupce"""
+c = c_r[0]
+"""radky"""
+r = c_r[1]
+
+"""
+INPUTS:
+24 24
+0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 1 1 0 0 0 0 0
+"""
+print(c)
+print(values)
