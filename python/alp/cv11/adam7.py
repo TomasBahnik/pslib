8e56aab6912a2aa10a595d1579b06ec1d4eb713d
pantom <pantom@centrum.cz>
2022-01-14 16:39:00 +0100

    adam7.py size

--- python/alp/cv11/adam7.py
+++ python/alp/cv11/adam7.py
@@ -1,14 +1,23 @@
 c_r = list(map(int, input().strip().split()))
 values = list(map(int, input().strip().split()))
+adam7 = [[1, 6, 4, 6, 2, 6, 4, 6],
+         [7, 7, 7, 7, 7, 7, 7, 7],
+         [5, 6, 5, 6, 5, 6, 5, 6],
+         [7, 7, 7, 7, 7, 7, 7, 7],
+         [3, 6, 4, 6, 3, 6, 4, 6],
+         [7, 7, 7, 7, 7, 7, 7, 7],
+         [5, 6, 5, 6, 5, 6, 5, 6],
+         [7, 7, 7, 7, 7, 7, 7, 7]]
+
 """sloupce"""
 c = c_r[0]
 """radky"""
 r = c_r[1]
 
-if c % 8 !=0:
+if c % 8 != 0:
     print("ERROR! zadej cislo delitelne 8")
     exit(1)
-if r % 8 !=0:
+if r % 8 != 0:
     print("ERROR! zadej cislo delitelne 8")
     exit(1)
 
@@ -19,12 +28,23 @@ INPUTS:
 """
 
 
-for col in range(c):
-    continue
-for row in range(r):
-    print()
-    for col in range(len(values)):
-        if values[col] == 1:
-            print("x", end=" ")
-        else:
-            print("", end=" ")
+# for col in range(c):
+#     continue
+# for row in range(r):
+#     print()
+#     for col in range(len(values)):
+#         if values[col] == 1:
+#             print("x", end=" ")
+#         else:
+#             print("", end=" ")
+
+def adam7_size(original, col, row):
+    col_div = col // 8
+    row_div = row // 8
+    output = [original] * (col_div * row_div)
+    # print(output)
+    # print(len(output))
+    return output
+
+
+adam7_size(adam7, c, r)
