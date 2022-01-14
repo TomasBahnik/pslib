827416bf0aec5196ec5021b134752a51a267755d
pantom <pantom@centrum.cz>
2022-01-14 13:16:31 +0100

    adam7.py output without color + input check(divide by 8)

--- python/alp/cv11/adam7.py
+++ python/alp/cv11/adam7.py
@@ -5,10 +5,26 @@ c = c_r[0]
 """radky"""
 r = c_r[1]
 
+if c % 8 !=0:
+    print("ERROR! zadej cislo delitelne 8")
+    exit(1)
+if r % 8 !=0:
+    print("ERROR! zadej cislo delitelne 8")
+    exit(1)
+
 """
 INPUTS:
 24 24
 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 1 1 0 0 0 0 0
 """
-print(c)
-print(values)
+
+
+for col in range(c):
+    continue
+for row in range(r):
+    print()
+    for col in range(len(values)):
+        if values[col] == 1:
+            print("x", end=" ")
+        else:
+            print("", end=" ")
