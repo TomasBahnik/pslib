0ef241bfbbafbc546a1d840bf69525b730bcc7b9
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-01-15 22:05:15 +0100

    move data and not used functions to separate files

---
+++ python/alp/cv11/adam7_alg.py
@@ -0,0 +1,86 @@
+from alp.cv11.adam7_data import quarter_24_8
+from alp.cv11.adam7_data import full_24_8
+
+adam7_pattern = [[1, 6, 4, 6, 2, 6, 4, 6],
+                 [7, 7, 7, 7, 7, 7, 7, 7],
+                 [5, 6, 5, 6, 5, 6, 5, 6],
+                 [7, 7, 7, 7, 7, 7, 7, 7],
+                 [3, 6, 4, 6, 3, 6, 4, 6],
+                 [7, 7, 7, 7, 7, 7, 7, 7],
+                 [5, 6, 5, 6, 5, 6, 5, 6],
+                 [7, 7, 7, 7, 7, 7, 7, 7]]
+
+
+def empty_image(w, h):
+    b = []
+    for i in range(h):
+        b.append([0] * w)
+    return b
+
+
+def idx_of_pass(p):
+    all_rows = []
+    for i in range(len(adam7_pattern)):
+        one_row = []
+        for j in range(len(adam7_pattern[i])):
+            if adam7_pattern[i][j] == p:
+                one_row += [[i, j]]
+        all_rows.append(one_row)
+    return all_rows
+
+
+idx_of_passes = {}
+for p in range(1, 8):
+    idx_of_passes[p] = idx_of_pass(p)
+
+
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+
+def adam7_decode(test_input):
+    img_w = test_input[0][0]
+    img_h = test_input[0][1]
+    decoded_image = empty_image(img_w, img_h)
+    img_coded = test_input[1]
+    w_r = img_w // 8
+    r_r = img_h // 8
+    row_range = [x * 8 for x in range(0, r_r)]
+    col_range = [x * 8 for x in range(0, w_r)]
+    all_samples = all_image_samples(col_range, row_range)
+    for i in range(len(img_coded)):
+        val = img_coded[i]
+        coord = all_samples[i]
+        decoded_image[coord[0]][coord[1]] = val
+    return decoded_image
+
+
+def all_image_samples(col_range, row_range):
+    all_samples = []
+    for p in range(1, 8):
+        samples = idx_of_passes[p]
+        for r in row_range:
+            for row_samples in samples:
+                for c in col_range:
+                    for row_sample in row_samples:
+                        n_r = row_sample[0] + r
+                        n_c = row_sample[1] + c
+                        all_samples += [[n_r, n_c]]
+    return all_samples
+
+
+def print_image(decoded_image):
+    for r in decoded_image:
+        s1 = str(r).replace('1', '*').replace('0', ' ').replace(',', '').replace('[', '').replace(']', '')
+        print(s1)
+
+
+if __name__ == "__main__":
+    # test_adam(24, 8)
+    # adam7_encode(input_3[1], image_3_orig)
+    print("1/4 24x8")
+    image = adam7_decode(quarter_24_8)
+    print_image(image)
+    print("full 24x8")
+    image = adam7_decode(full_24_8)
+    print_image(image)
---
+++ python/alp/cv11/adam7_functions.py
@@ -0,0 +1,79 @@
+import sys
+
+from alp.cv11.adam7_alg import column, idx_of_passes, empty_image
+
+
+def adam7_encode(input_data, orig_image):
+    zeroes = input_data.count(0)
+    ones = input_data.count(1)
+    img_zeroes = 0
+    img_ones = 0
+    for r in orig_image:
+        img_zeroes += r.count(0)
+        img_ones += r.count(1)
+    if zeroes != img_zeroes or ones != img_ones:
+        print("ERROR : pocty nul a jednicek jsou ruzne")
+        sys.exit(1)
+    else:
+        print("pocty nul a jednicek jsou stejne")
+    img_w = len(orig_image[0])
+    img_h = len(column(orig_image, 0))
+    w_r = img_w // 8
+    r_r = img_h // 8
+    row_range = [x * 8 for x in range(0, r_r)]
+    col_range = [x * 8 for x in range(0, w_r)]
+    encoded_image = []
+    for p in range(1, 8):
+        samples = idx_of_passes[p]
+        for r in row_range:
+            for row_samples in samples:
+                for c in col_range:
+                    for row_sample in row_samples:
+                        n_r = row_sample[0] + r
+                        n_c = row_sample[1] + c
+                        encoded_image.append(orig_image[n_r][n_c])
+    if len(encoded_image) != len(input_data):
+        print("ERROR encoded image has different length")
+    if encoded_image.count(0) != input_data.count(0):
+        print("ERROR encoded image has different number of 0")
+    if encoded_image.count(1) != input_data.count(1):
+        print("ERROR encoded image has different number of 1")
+    else:
+        print("encoded image has {} of 1s".format(encoded_image.count(1)))
+        print("encoded image has {} of 0s".format(encoded_image.count(0)))
+        print("input data has {} of 1s".format(input_data.count(1)))
+        print("input data has {} of 0s".format(input_data.count(0)))
+    if encoded_image != input_data:
+        print("ERROR encoded image is different from provided input")
+        print("encoded image {}".format(encoded_image))
+        print("input data {}".format(input_data))
+    else:
+        print("encoded image == input data!!")
+
+
+def test_adam(img_w, img_h):
+    e_i = empty_image(img_w, img_h)
+    w_r = img_w // 8
+    r_r = img_h // 8
+    row_range = [x * 8 for x in range(0, r_r)]
+    col_range = [x * 8 for x in range(0, w_r)]
+    all_samples = []
+    pruchody = []
+    for p in range(1, 8):
+        samples = idx_of_passes[p]
+        for r in row_range:
+            for c in col_range:
+                for s in samples:
+                    n_r = s[0] + r
+                    n_c = s[1] + c
+                    pruchody.append(p)
+                    all_samples += [[n_r, n_c]]
+    # all_samples.sort(key=lambda x: (x[0], x[1]))
+    if len(all_samples) != len(pruchody):
+        print("ERROR - length mismatch exiting")
+        sys.exit(1)
+    for i in range(len(all_samples)):
+        coord = all_samples[i]
+        pruchod = pruchody[i]
+        e_i[coord[0]][coord[1]] = pruchod
+    print(e_i)
