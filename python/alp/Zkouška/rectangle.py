fdda90184abc2241cfa19eaee37e6d781e0a69bb
pantom <pantom@centrum.cz>
2022-01-28 09:25:56 +0100

    zkouska2

--- python/alp/Zkouška/rectangle.py
+++ python/alp/Zkouška/rectangle.py
@@ -1,28 +1,33 @@
 import sys
+
 RED = 0
 GREEN = 1
 
-def load_lines_matrix(file):
+
+def load_input(file):
     pole = []
     with open(file, 'r') as f:
         for line in f:
             pole.append(list(map(int, line.rstrip().split())))
-    print(pole)
+    # print(pole)
     return pole
 
 
-# def load_char_matrix(file):
-#     pole = []
-#     with open(file, 'r') as f:
-#         for line in f:
-#             chars = [char for char in line.rstrip()]
-#             pole.append(chars)
-#     print(pole)
-#     return pole
+vstup = load_input(sys.argv[1])
+red_points = []
+for i in range(len(vstup)):
+    if vstup[i][2] == 0:
+        red_points.append(vstup[i][:2])
+print(red_points)
 
+green_points = []
+for i in range(len(vstup)):
+    if vstup[i][2] == 1:
+        green_points.append(vstup[i][:2])
+print(green_points)
 
 
 
 if __name__ == '__main__':
-    load_lines_matrix(sys.argv[1])
+    load_input(sys.argv[1])
     # load_char_matrix(sys.argv[1])
