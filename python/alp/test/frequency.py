e77b46491d32ea09a7b1eb9544bd6be943d582ab
pantom <pantom@centrum.cz>
2022-02-07 16:46:42 +0100

    FINAL. (doupravim ten main at je to prehlednejsi)

--- python/alp/test/frequency.py
+++ python/alp/test/frequency.py
@@ -9,7 +9,7 @@ def count_char_in_word(word, char):
             shift = word.index(char, shift + 1)
             count += 1
         except ValueError as ve:
-            print(ve)
+            # print(ve)
             break
     return count
 
@@ -30,9 +30,8 @@ def is_empty(file):
         sys.exit(0)
 
 
-def words_contains_letters(file):
+def words_contains_letters(file, inp):
     p = load_input(file)
-    inp = input()  # POLE!!
     pole = []
     for slovo in p:
         count = 0
@@ -43,23 +42,39 @@ def words_contains_letters(file):
                     count += 1
                 if count == len(inp):
                     count = 0
-                    print(slovo)
+                    # print(slovo)
                     pole.append(slovo)
-    return pole
+    return pole, inp
 
 
-def test_output(slovo, znak):
-    print("'{}' in '{}' : {}x ".format(znak, slovo, count_char_in_word(slovo, znak)))
+# def test_output(slovo, znak):
+#     print("'{}' in '{}' : {}x ".format(znak, slovo, count_char_in_word(slovo, znak)))
+#
+#
+# def test():
+#     test_output("ahghiiimasftrgssktejj", 'j')
+#     test_output("ahghiiimasftirgsisktejj", 'i')
 
 
-def test():
-    test_output("ahghiiimasftrgssktejj", 'j')
-    test_output("ahghiiimasftirgsisktejj", 'i')
+def max_contained_word(words, inp):
+    compare = []
+    for i in range(0, len(words)):
+        counter = []
+        for j in range(0, len(inp)):
+            counter.append(count_char_in_word(words[i], inp[j]))
+        compare.append(sum(counter))
+    max_value = max(compare)
+    max_index = compare.index(max_value)
+    # print(words[max_index])
+    return words[max_index]
 
 
 if __name__ == '__main__':
-    # is_empty(sys.argv[1])
     # words_contains_letters(sys.argv[1])
-    print(len(words_contains_letters(sys.argv[1])))
     # test()
+    inp_txt = sys.argv[1]
+    inp = input()
+    is_empty(inp_txt)
+    print(len(words_contains_letters(inp_txt, inp)[0]))
+    print(max_contained_word(words_contains_letters(inp_txt, inp)[0], words_contains_letters(inp_txt, inp)[1]))
     sys.exit(0)
