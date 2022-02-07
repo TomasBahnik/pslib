bf1d7f4308cad6adf7f42fffcec6d7b44cc0d369
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-02-07 14:33:26 +0100

    my full solution

--- python/alp/test/frequency.py
+++ python/alp/test/frequency.py
@@ -9,9 +9,27 @@ def count_char_in_word(word, char):
             shift = word.index(char, shift + 1)
             count += 1
         except ValueError as ve:
-            print(ve)
             break
     return count
+
+
+def word_contains_letters(word, letters):
+    total_count = 0
+    for letter in letters:
+        cnt = count_char_in_word(word, letter)
+        if cnt == 0:
+            return 0
+        else:
+            total_count += cnt
+    return total_count
+
+
+def words_letters(file, letters):
+    words = load_input(file)
+    w_l = []
+    for word in words:
+        w_l.append([word, word_contains_letters(word, letters)])
+    return w_l
 
 
 def load_input(file):
@@ -52,14 +70,23 @@ def test_output(slovo, znak):
     print("'{}' in '{}' : {}x ".format(znak, slovo, count_char_in_word(slovo, znak)))
 
 
-def test():
+def test_1():
     test_output("ahghiiimasftrgssktejj", 'j')
     test_output("ahghiiimasftirgsisktejj", 'i')
+
+
+def test(file, letters):
+    x = words_letters(file, letters)
+    c = [a[0] for a in x if a[1] > 0]
+    m = [a[1] for a in x if a[1] > 0]
+    max_cnt_idx = m.index(max(m))
+    print(letters, "contained in :", c, "delka:", len(c))
+    print(letters, "maximal count :", c[max_cnt_idx])
 
 
 if __name__ == '__main__':
     # is_empty(sys.argv[1])
     # words_contains_letters(sys.argv[1])
     print(len(words_contains_letters(sys.argv[1])))
-    # test()
+    # test(sys.argv[1], 'irg')
     sys.exit(0)
