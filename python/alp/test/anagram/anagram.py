534fb55945845a89b050153405bd1bf5f0e30b08
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-02-08 13:25:03 +0100

    add anagram test function with test cases

--- python/alp/test/anagram/anagram.py
+++ python/alp/test/anagram/anagram.py
@@ -45,6 +45,30 @@ priklady anagramu :
 import sys
 
 
+def remove_spaces(s1):
+    tmp = s1
+    tmp = tmp.strip()
+    tmp = tmp.replace(' ', '')
+    return tmp
+
+
+def is_anagram(s1, s2):
+    return sorted(remove_spaces(s1)) == sorted(remove_spaces(s2))
+
+
+def test(s1, s2):
+    print("{} and {} are anagram {}".format(s1, s2, is_anagram(s1, s2)))
+
+
+def all_tests():
+    test('abc', 'a b c')
+    test('abc', 'a c b')
+    test('forty five', 'over fifty')
+    test('forty five', 'fiftyover')
+    test('Ac', 'CA')
+    test('Ac', 'cA')
+
+
 def load_input(file):
     pole = []
     with open(file, 'r') as f:
@@ -56,3 +80,5 @@ def load_input(file):
 
 if __name__ == '__main__':
     load_input(sys.argv[1])
+    # all_tests()
+    sys.exit(0)
