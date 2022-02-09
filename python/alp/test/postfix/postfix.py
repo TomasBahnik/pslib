732aecad66b47ab2844fdbc60c725e569772d7bf
pantom <pantom@centrum.cz>
2022-02-09 16:03:38 +0100

    add function : if len(words) = 0 ---> NONE

--- python/alp/test/postfix/postfix.py
+++ python/alp/test/postfix/postfix.py
@@ -22,14 +22,14 @@ def shortest_word(words):
     sequence = words_len.index(min(words_len))
     return words[sequence]
 
+
 if __name__ == '__main__':
     file = sys.argv[1]
     inp = input()
     # load_input(file)
     w_w_i = words_with_inp(inp)
     print(len(w_w_i))
+    if len(w_w_i) == 0:
+        print("NONE")
+        sys.exit(0)
     print(shortest_word(w_w_i))
-
-
-
-
