e319367e0873a6085351c2e2093414fee1e22f7c
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-02-11 10:36:18 +0100

    initial add maze

---
+++ python/alp/test/maze/maze_solver.py
@@ -0,0 +1,30 @@
+import sys
+
+files = ['maze1.txt', 'maze1.txt', 'maze2.txt']
+
+reseni = ['', '', '', ]
+
+
+def load_matrix(file):
+    pole = []
+    with open(file, 'r') as f:
+        for line in f:
+            pole.append(list(map(int, line.split())))
+    return pole
+
+
+def test_maze(file):
+    print(file)
+    board = load_matrix(file)
+    print(board)
+
+
+def all_test():
+    for f in files:
+        test_maze(f)
+
+
+if __name__ == '__main__':
+    all_test()
+    sys.exit(0)
+
