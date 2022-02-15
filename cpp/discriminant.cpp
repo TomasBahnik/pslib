6426a4263ead32369ab3b0b8e07bf81b097d11bb
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-02-15 18:18:53 +0100

    working C code from pslib

--- cpp/discriminant.cpp
+++ cpp/discriminant.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <math.h>       /* sqrt */
 
-int main ()
-{
-	float a,b,c,result,Fi,sinus;
-	a=1;
-	b=5;
-	c=5;
-	Fi=3.14;
-	sinus=sin(Fi);
-	result = sqrt (b*b - 4*a*c);
-	printf ("b^2 = %f, 4ac = %f, determinant = %f\n", b*b, 4*a*c, result);
-	printf ("sinus %f = %f\n", Fi, sinus);
-	return 0;
+int main() {
+    float a, b, c, result, Fi, sinus;
+    a = 1;
+    b = 5;
+    c = 5;
+    Fi = 3.14;
+    sinus = sin(Fi);
+    result = sqrt(b * b - 4 * a * c);
+    printf("b^2 = %f, 4ac = %f, determinant = %f\n", b * b, 4 * a * c, result);
+    printf("sinus %f = %f\n", Fi, sinus);
+    return 0;
 }
