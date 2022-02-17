0b6b855fbf029d0531a294adabed76c00515e5f1
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-02-17 09:58:19 +0100

    add cmake file and extended std in

--- cpp/fel/std_in.c
+++ cpp/fel/std_in.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 // input 1-20.3-4.0e3
 int main() {
     int i, j;
@@ -8,5 +9,16 @@ int main() {
     printf("%d\n", j);
     printf("%f\n", x);
     printf("%f\n", y);
+
+    printf(" d/d/==== \n");
+    scanf("%d/%d", &i, &j);
+    printf("%d\n", i);
+    printf("%d\n", j);
+
+    printf(" stdin ==== \n");
+    // use inout.log
+    fscanf(stdin, "%d/%d", &i, &j);
+    printf("%d\n", i);
+    printf("%d\n", j);
     return 0;
 }
