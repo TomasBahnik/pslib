942b4fca20ca46b9dba1b6a698d6ee5eae48049b
pantom <pantom@centrum.cz>
2022-02-20 13:28:02 +0100

    HW01_KUI_final

--- python/KUI/prohledavani_1/solvability_check.py
+++ python/KUI/prohledavani_1/solvability_check.py
@@ -4,12 +4,60 @@ import npuzzle
 def is_solvable(env):
     '''
     True or False?
-    Tady naprogramujte svoje reseni
     '''
+    env_line = []
+    count = 0
+    space_idx = int
+    try:
+        env.read_tile(3, 3)
+        for i in range(4):
+            for j in range(4):
+                if env.read_tile(i, j) is None:
+                    env_line.append(16)
+                    space_idx = 4 - i
+                else:
+                    env_line.append(env.read_tile(i, j))
+        for i in range(len(env_line)):
+            for j in range(i + 1, len(env_line)):
+                if env_line[i] > env_line[j]:
+                    count += 1
+        if count % 2 == 0:
+            odd_even_inv = 0  # inversion is even
+        else:
+            odd_even_inv = 1  # inversion is odd
+
+        if space_idx % 2 == 0:
+            odd_even_space_idx = 0  # space idx is even
+        else:
+            odd_even_space_idx = 1  # space idx is odd
+
+        if odd_even_inv == 0 and odd_even_space_idx == 1:
+            return True
+        if odd_even_inv == 1 and odd_even_space_idx == 0:
+            return True
+        else:
+            return False
+    except:
+        for i in range(3):
+            for j in range(3):
+                if env.read_tile(i, j) is None:
+                    env_line.append(9)
+                else:
+                    env_line.append(env.read_tile(i, j))
+        for i in range(len(env_line)):
+            for j in range(i + 1, len(env_line)):
+                if env_line[i] > env_line[j]:
+                    count += 1
+        if count % 2 == 0:
+            odd_even_inv = 0  # inversion is even
+            return True
+        else:
+            odd_even_inv = 1  # inversion is odd
+            return False
 
 
 if __name__ == "__main__":
-    env = npuzzle.NPuzzle(3)  # env = npuzzle.NPuzzle(4)
+    env = npuzzle.NPuzzle(4)  # env = npuzzle.NPuzzle(4)
     env.reset()
     env.visualise()
     # just check
