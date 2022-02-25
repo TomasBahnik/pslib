f4f9053e2fe9f9025fda9962375b21fc3e26f1e7
pantom <pantom@centrum.cz>
2022-02-25 11:25:24 +0100

    add tests of house and fence dimension

--- cpp/fel/HW01/b3b36prg-hw01/main.c
+++ cpp/fel/HW01/b3b36prg-hw01/main.c
@@ -4,6 +4,12 @@
 
 #define ERROR_WRONG_INPUT 100;
 
+int Test_house_dim(int, int);
+int Test_fence_dim(int);
+
+int Print_house(int, int);
+int Print_fence(int, int, int);
+
 int main(int argc, char *argv[])
 {
     int ret = 0;
@@ -24,5 +30,36 @@ int main(int argc, char *argv[])
         printf("House dim is: %d x %d + %d\n", w, h, f_w);
     }
 
+    return ret;
+}
+
+int Print_house(int w, int h)
+{
+    int ret = Test_house_dim(w, h);
+    if (ret == 0) {
+    }
+    return ret;
+}
+
+int Print_fence(int w, int h, int fw)
+{
+    int ret = Test_house_dim(w, h);
+    if (ret == 0) {
+        ret = Test_fence_dim(fw);
+    }
+    if (ret == 0) {
+    }
+    return ret;
+}
+
+int Test_house_dim(int w, int h)
+{
+    int ret = 0;
+    return ret;
+}
+
+int Test_fence_dim(int fw)
+{
+    int ret = 0;
     return ret;
 }
