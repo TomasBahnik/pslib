dbe4388a5c63707333101c60f3ff77c4873b46c6
toba <toba@spoluzaci.cz>
2022-02-28 21:26:42 +0100

    add test data and test script

---
+++ cpp/fel/HW01/b3b36prg-hw01/tests/test_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+LOGGER_TIMEFORMAT='+%Y-%m-%dT%H:%M:%S%z'
+function message() {
+  printf "%s : %s\n" "$(date $LOGGER_TIMEFORMAT)" "$1"
+}
+
+TEST_FILES=(*.txt)
+message "compiling : clang main.c -o main"
+clang ../main.c -o main
+message "running ${#TEST_FILES[@]} tests"
+
+FORMAT="%-12s %-10s %s\n"
+
+printf "%s\n"    "------------------------------"
+# shellcheck disable=SC2059
+printf "$FORMAT" "test_data" "exit_code" "output"
+printf "%s\n"    "------------------------------"
+
+for test_file in "${TEST_FILES[@]}"; do
+  test_data=$(<"./$test_file")
+  output=$(./main <"$test_file")
+  exit_code=$?
+  # shellcheck disable=SC2059
+  printf "$FORMAT" "[$test_data]" "$exit_code" "$output"
+done
