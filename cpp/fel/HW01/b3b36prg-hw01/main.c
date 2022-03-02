60aa6fdee8108fa1aaf8f07134ea2fdf0c932493
toba <toba@spoluzaci.cz>
2022-03-02 13:31:14 +0100

    refactor main.c * single switch in main function (default branch added just fo sure) * validation is done before printing when reading input * print functions do not any validation

--- cpp/fel/HW01/b3b36prg-hw01/main.c
+++ cpp/fel/HW01/b3b36prg-hw01/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NO_ERROR 0
 #define ERROR_WRONG_INPUT 100
 #define ERROR_HOUSE_DIM_OUT_OF_RANGE 101
 #define ERROR_HOUSE_WITH_IS_NOT_ODD 102
@@ -22,17 +23,9 @@ int read_input(int *w, int *h, int *f_w);
 
 int main(int argc, char *argv[])
 {
-    int ret = 0;
+    int ret = NO_ERROR;
     int w, h, f_w;
-    switch (ret = read_input(&w, &h, &f_w)) {
-    case MANDATORY:
-        ret = print_roof(w, h);
-        ret = print_house(w, h);
-        break;
-    case OPTIONAL:
-        ret = print_fence(w, h, f_w);
-        break;
-    }
+    ret = read_input(&w, &h, &f_w);
     switch (ret) {
     case ERROR_WRONG_INPUT:
         fprintf(stderr,
@@ -41,104 +34,138 @@ int main(int argc, char *argv[])
     case ERROR_HOUSE_DIM_OUT_OF_RANGE:
         fprintf(stderr, "Error: Vstup mimo interval!\n");
         break;
-
     case ERROR_HOUSE_WITH_IS_NOT_ODD:
         fprintf(stderr, "Error: Sirka neni liche cislo!\n");
         break;
-
     case ERROR_FENCE_WIDTH_INVALID:
         fprintf(stderr, "Error: Neplatna velikost plotu!\n");
+        break;
+    /*
+     * All errors handled => only printing.
+     * MANDATORY is value 0 same as NO_ERROR
+     * OPTIONAL is value 1
+     * It would be better to use some defined constants
+     * But in previous cases 0 (NO_ERROR) and 1 are not checked so this the
+     * only place where 0 and 1 can fall
+     */
+    case MANDATORY:
+        print_roof(w, h);
+        ret = print_house(w, h);
+        break;
+    case OPTIONAL:
+        ret = print_fence(w, h, f_w);
+        break;
+    // Just for sure
+    default:
+        printf("Default case");
         break;
     }
     return ret;
 }
 
+/*
+ * Validate input before plotting anything
+ */
 int read_input(int *w, int *h, int *f_w)
 {
     int ret = ERROR_WRONG_INPUT;
+    int test_dims_code = NO_ERROR;
     if (scanf("%i %i", w, h) == 2) {
+        test_dims_code = test_house_dim(*w, *h);
+        if (test_dims_code != NO_ERROR) {
+            return test_dims_code;
+        }
+        // width and height are OK
         ret = MANDATORY;
     }
-    if (ret == MANDATORY && *w == *h &&
-        scanf("%i", f_w) == 1) { // overi jestli je 3 vstup cele cislo
+    //decides if read also fence width
+    bool read_fence_width = ret == MANDATORY && *w == *h;
+    if (read_fence_width) {
+        bool fence_width_value_ok = scanf("%i", f_w) == 1;
+        if (!fence_width_value_ok) {
+            //wrong fence width return immediately
+            return ERROR_WRONG_INPUT;
+        }
+        test_dims_code = test_fence_dim(*h,*f_w);
+        if (test_dims_code != NO_ERROR) {
+            return test_dims_code;
+        }
         ret = OPTIONAL;
     }
     return ret;
 }
 
+/*
+ * Only printing, no validity checks
+ */
 int print_roof(int w, int h)
 {
-    int ret = test_house_dim(w, h);
-    if (ret == 0) {
-        int roof_height = (w - 1) / 2; // height of roof
-        for (int i = 0; i < roof_height; ++i) {
-            for (int j = 0; j < roof_height + i + 1; ++j) {
-                if ((j == roof_height + i) || ((j == roof_height - i)))
-                    printf("X");
-                else
-                    printf(" ");
-            }
-            printf("\n");
+    int roof_height = (w - 1) / 2; // height of roof
+    for (int i = 0; i < roof_height; ++i) {
+        for (int j = 0; j < roof_height + i + 1; ++j) {
+            if ((j == roof_height + i) || ((j == roof_height - i)))
+                printf("X");
+            else
+                printf(" ");
         }
+        printf("\n");
     }
-    return 1;
+    return NO_ERROR;
 }
 
+/*
+ * Only printing, no validity checks
+ */
 int print_house(int w, int h)
 {
-    int ret = test_house_dim(w, h);
-    if (ret == 0) {
-        // printf("House dim is: %d x %d\n", w, h);
-        for (int i = 0; i < h; ++i) {
-            for (int j = 0; j < w; ++j) {
-                if ((i == 0) || (i == h - 1))
-                    printf("X");
-                if ((j == 0) && (i >= 1) && (i < h - 1))
-                    printf("X");
-                if ((j < w - 2) && (i >= 1) && (i < h - 1))
-                    printf(" ");
-                if ((j == w - 1) && (i >= 1) && (i < h - 1))
-                    printf("X");
-            }
-            printf("\n");
+    // printf("House dim is: %d x %d\n", w, h);
+    for (int i = 0; i < h; ++i) {
+        for (int j = 0; j < w; ++j) {
+            if ((i == 0) || (i == h - 1))
+                printf("X");
+            if ((j == 0) && (i >= 1) && (i < h - 1))
+                printf("X");
+            if ((j < w - 2) && (i >= 1) && (i < h - 1))
+                printf(" ");
+            if ((j == w - 1) && (i >= 1) && (i < h - 1))
+                printf("X");
         }
+        printf("\n");
     }
-    return ret;
+    return NO_ERROR;
 }
 
+/*
+ * Only printing, no validity checks
+ */
 int print_fence(int w, int h, int f_w)
 {
-    int ret = test_house_dim(w, h);
-    if (ret == 0) {
-        ret = test_fence_dim(h, f_w);
-    }
-    if (ret == 0) { // all test passed
-        printf("House dim is: %d x %d + %d\n", w, h, f_w);
-    }
-    return ret;
+    printf("Print fence for house with dim: %d x %d + %d\n", w, h, f_w);
+    return NO_ERROR;
 }
 
 int test_house_dim(int w, int h)
 {
-    int ret = 0;
+    int ret = NO_ERROR;
     int dim_ok = (house_dim_min <= w) && (w <= house_dim_max) &&
                  (house_dim_min <= h) && (h <= house_dim_max);
     if (!dim_ok) {
         ret = ERROR_HOUSE_DIM_OUT_OF_RANGE;
+        // test on invalid dimension goes before test of width
+        return ret;
     }
     if (w % 2 == 0) {
         ret = ERROR_HOUSE_WITH_IS_NOT_ODD;
+        return ret;
     }
     return ret;
 }
 
 int test_fence_dim(int h, int f_w)
 {
-    int ret = 0;
     if (f_w > 0 && f_w < h) {
-        // OK
+        return NO_ERROR;
     } else {
-        ret = ERROR_FENCE_WIDTH_INVALID;
+        return ERROR_FENCE_WIDTH_INVALID;
     }
-    return ret;
 }
