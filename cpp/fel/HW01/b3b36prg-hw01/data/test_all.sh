0f46cf6694ee6947ddb70eb1f944de8efd969139
toba <toba@spoluzaci.cz>
2022-03-02 19:04:04 +0100

    working test of equal errors, output file as in zadani

--- cpp/fel/HW01/b3b36prg-hw01/data/test_all.sh
+++ cpp/fel/HW01/b3b36prg-hw01/data/test_all.sh
@@ -5,28 +5,38 @@ function message() {
 }
 
 TEST_FILES=(pub*.in)
-message "compiling with 'clang -pedantic -Wall -Werror -std=c99 ../main.c -o main'"
-clang -pedantic -Wall -Werror -std=c99 ../main.c -o main
+OUTPUT_FILE=b3b36prg-hw01
+message "compiling with 'clang -pedantic -Wall -Werror -std=c99 ../main.c -o $OUTPUT_FILE'"
+clang -pedantic -Wall -Werror -std=c99 ../main.c -o $OUTPUT_FILE
 message "running ${#TEST_FILES[@]} tests"
 
-FORMAT="%-15s %-10s %s\n"
-FORMAT_OUT="%-15s %-10s \n\n%s\n\n"
+FORMAT="%-15s %-10s %-10s %s\n"
+FORMAT_OUT="%-15s %-10s \n\n%s \n%s\n\n"
 
-printf "%s\n" "------------------------------"
+printf "%s\n" "--------------------------------------------------"
 # shellcheck disable=SC2059
-printf "$FORMAT" "test_data" "exit_code" "error"
-printf "%s\n" "------------------------------"
+printf "$FORMAT" "test_data" "exit_code" "error" "expected_error"
+printf "%s\n" "--------------------------------------------------"
 
-for test_file in "${TEST_FILES[@]}"; do
-  test_data=$(<"./$test_file")
-  error="$(./main <"$test_file" 2>&1 >/dev/null)"
+for test_input in "${TEST_FILES[@]}"; do
+  test_output=${test_input//in/out}
+  test_output_content=$(cat "$test_output")
+  test_error=${test_input//in/err}
+  test_err_content=$(cat "$test_error")
+  test_data=$(<"./$test_input")
+  error="$(./$OUTPUT_FILE <"$test_input" 2>&1 >/dev/null)"
   exit_code=$?
   if ((exit_code > 0)); then
+    if [[ "$error" == "$test_err_content" ]]; then
+      res="PASS"
+    else
+      res="FAIL"
+    fi
     # shellcheck disable=SC2059
-    printf "$FORMAT" "[$test_data]" "$exit_code" "$error"
+    printf "$FORMAT" "[$test_data]" "$exit_code" "$error" "$res"
   else
-    output="$(./main <"$test_file")"
+    output="$(./$OUTPUT_FILE <"$test_input")"
     # shellcheck disable=SC2059
-    printf "$FORMAT_OUT" "[$test_data]" "$exit_code" "$output"
+    printf "$FORMAT_OUT" "[$test_data]" "$exit_code" "$output" "$test_output_content"
   fi
 done
