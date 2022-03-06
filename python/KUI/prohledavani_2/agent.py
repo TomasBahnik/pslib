d1d025bd50a9414547920a8813753a5c9891efe4
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-03-06 11:16:15 +0100

    rename aima agent to agent.py and remove the others

--- python/KUI/prohledavani_2/agent.py
+++ python/KUI/prohledavani_2/agent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import math
+import sys
 import time
 
 import kuimaze
@@ -39,6 +40,23 @@ def return_path(end_node):
     return list(reversed(ret_val))
 
 
+# function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
+#   node ← a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
+#   frontier ← a priority queue ordered by PATH-COST, with node as the only element
+#   explored ← an empty set
+#   loop do
+#     if EMPTY?(frontier) then return failure
+#     node ← POP(frontier) /* chooses the lowest-cost node in frontier */
+#     if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+#     add node.STATE to explored
+#     for each action in problem.ACTIONS(node.STATE) do
+#       child ← CHILD-NODE(problem,node,action)
+#       if child.STATE is not in explored or frontier then
+#           frontier ← INSERT(child,frontier)
+#       else if child.STATE is in frontier with higher PATH-COST then
+#           replace that frontier node with child
+
+
 class Agent(kuimaze.BaseAgent):
     """
     Simple example of agent class that inherits kuimaze.BaseAgent class
@@ -59,74 +77,61 @@ class Agent(kuimaze.BaseAgent):
         start_node.g = 0
         start_node.f = start_node.g + start_node.h
         end_node.g = end_node.h = end_node.f = 0
-        frontiers = [start_node]  # init by start node
+        frontier = [start_node]  # init by start node
         explored = []
 
-        # Adding a stop condition (infinite loops or long time.)
-        # TODO remove before submit
-        iteration = 0
-        max_iterations = (self.environment._xsize // 2) ** 10
-
-        while len(frontiers) > 0:
+        while True:
+            if len(frontier) == 0:
+                return None
             # sort by evaluation function
-            frontiers.sort(key=lambda node: node.f)
+            frontier.sort(key=lambda node: node.f)
             # remove and get node with smallest f
-            current_node = frontiers.pop(0)
-
-            # TODO remove before submit
-            iteration += 1
-            if iteration > max_iterations:
-                print("too many iterations")
-                return return_path(current_node)
-
-            explored.append(current_node)
-
+            current_node = frontier.pop(0)
             # test if goal is reached or not, if yes then return the path
             if current_node == end_node:
                 return return_path(current_node)
-
-            # Generate children [[(x1, y1), cost], [(x2, y2), cost], ... ]
+            explored.append(current_node)
             expanded = self.environment.expand(current_node.state)
-
             for child in expanded:
                 state = child[0]
                 child_node = Node(current_node, state)
-                if child_node in explored:
-                    continue
                 step_cost = child[1]
                 child_node.g = step_cost + current_node.g
                 child_node.h = euclidean_distance(child_node, end_node)
                 child_node.f = child_node.g + child_node.h
-                # Child is already in the frontiers and there is lower cost
-                # TODO simplify
-                if len([node for node in frontiers if child_node == node and child_node.f > node.f]) > 0:
-                    continue
-                frontiers.append(child_node)
-
+                # if child.STATE is not in explored or frontier then
+                # frontier ← INSERT(child,frontier)
+                if (child_node not in explored) and (child_node not in frontier):  # state is used for comparison
+                    frontier.append(child_node)
+                # else if child.STATE is in frontier with higher PATH-COST then
+                # TODO check len must be 0 or 1
+                elif len([node for node in frontier if child_node == node and node.f > child_node.f]) > 0:
+                    # replace that frontier node with child
+                    frontier_states = [x.state for x in frontier]
+                    state_idx = frontier_states.index(child_node.state)
+                    frontier[state_idx] = child_node
             # show environment GUI
             # TODO DO NOT FORGET TO COMMENT THIS LINE BEFORE FINAL SUBMISSION!
-            self.environment.render()
+            if SHOW:
+                self.environment.render()
             # sleep for demonstration
             # TODO DO NOT FORGET TO COMMENT THIS LINE BEFORE FINAL SUBMISSION!
-            time.sleep(0.2)
-        return None
+            # time.sleep(0.01)
 
 
+SHOW = None  # enables env GUI and show the resulting image for SHOW sec
 if __name__ == '__main__':
-    MAP = 'maps/normal/normal10b.bmp'
+    MAP = sys.argv[1]  # 'maps/normal/normal11.bmp'
+    SHOW = int(sys.argv[2]) if len(sys.argv) > 2 else None
     GRAD = (0, 0)
-    SAVE_PATH = False
-    SAVE_EPS = False
-
     env = kuimaze.InfEasyMaze(map_image=MAP, grad=GRAD)  # For using random map set: map_image=None
     agent = Agent(env)
-
+    t0 = time.perf_counter()
     path = agent.find_path()
+    delta = time.perf_counter() - t0
     print(path)
-    env.set_path(path)  # set path it should go from the init state to the goal state
-    if SAVE_PATH:
-        env.save_path()  # save path of agent to current directory
-    if SAVE_EPS:
-        env.save_eps()  # save rendered image to eps
-    env.render(mode='human')
-    time.sleep(3)
+    print("{} : path len {}, takes {} sec".format(MAP, len(path), delta))
+    if SHOW:
+        env.set_path(path)  # set path it should go from the init state to the goal state
+        env.render(mode='human')
+        time.sleep(SHOW)
