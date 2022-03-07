a13b39816022024ce0cb139b343b2541c30b1e8a
pantom <pantom@centrum.cz>
2022-03-07 19:43:53 +0100

    add fce that print prime numbers to 10 ** 6

--- cpp/fel/HW02/b3b36prg-hw02/main.c
+++ cpp/fel/HW02/b3b36prg-hw02/main.c
@@ -1,10 +1,16 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ERROR_INPUT 100
+#define milion 1000000 // milion is 10 ** 6
+int array[milion] = {0};
 // solution 1 // mandatory
 // deviding input by smaller number than input number
+
 int read_number(void);
-int decompose(int n);
+void decompose(int n);
+void prime_numbers();
 
 // solution 2 // optional
 // dividing by smaller numbers && algorithm end when dividend is smaller than
@@ -12,20 +18,20 @@ int decompose(int n);
 
 // solution 3 // optional
 // Sieve of Eratosthenes
-enum { ERROR_INPUT = 100 };
 
 int main(int argc, char *argv[])
 {
     int ret = EXIT_SUCCESS;
     int n;
-    while ((n = read_number()) > 0) {
-        printf("Prvociselny rozklad cisla %d je:\n", n);
-        decompose(n);
-    }
+    // while ((n = read_number()) > 0) {
+    //     printf("Prvociselny rozklad cisla %d je:\n", n);
+    //     decompose(n);
+    // }
     if (n < 0) {
         fprintf(stderr, "Error: Chybny vstup!\n");
         return ERROR_INPUT;
     }
+    prime_numbers();
     return ret;
 }
 
@@ -38,8 +44,30 @@ int read_number(void)
     return n;
 }
 
-int decompose(int n)
+void prime_numbers()
 {
-    printf("rozklad\n");
-    return 0;
+    for (int i = 2; i < milion; ++i) {
+        if (array[i] == 0) {
+            for (int j = i; j < milion; j += i) {
+                array[j] = 1;
+            }
+            printf("%d\n", i);
+        }
+    }
 }
+// void decompose(int n)
+// {
+//     int array[milion] = {0};
+//     array[2] = 1;
+//     for (int i = 2; i < milion; ++i) {
+//         if (array[i] == 0) {
+//             for (int j = 2; j < milion; ++j) {
+//                 if (j % i == 0) {
+//                     array[j] = 1;
+//                 }
+//             }
+//         }
+//         if (array[i] == 0)
+//             printf("%d\n", i);
+//     }
+// }
