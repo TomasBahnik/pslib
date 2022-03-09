dc6cd7e48df60e5ee54cb992929a65fc29c115d6
pantom <pantom@centrum.cz>
2022-03-09 18:34:40 +0100

    DONE

--- cpp/fel/HW02/b3b36prg-hw02/main.c
+++ cpp/fel/HW02/b3b36prg-hw02/main.c
@@ -10,7 +10,7 @@
 // deviding input by smaller number than input number
 
 int read_number(void);
-void decompose(int n);
+void decompose(int n, int primes[]);
 void prime_numbers(int primes[]);
 
 // solution 2 // optional
@@ -25,15 +25,15 @@ int main(int argc, char *argv[])
     int primes[primes_in_mil];
     int ret = EXIT_SUCCESS;
     int n;
-    // while ((n = read_number()) > 0) {
-    //     printf("Prvociselny rozklad cisla %d je:\n", n);
-    //     decompose(n);
-    // }
+    while ((n = read_number()) > 0) {
+        printf("Prvociselny rozklad cisla %d je:\n", n);
+        prime_numbers(primes);
+        decompose(n, primes);
+    }
     if (n < 0) {
         fprintf(stderr, "Error: Chybny vstup!\n");
         return ERROR_INPUT;
     }
-    prime_numbers(primes);
     return ret;
 }
 
@@ -60,16 +60,32 @@ void prime_numbers(int primes[])
             // printf("%d\n", i);
         }
     }
-    printf("%d\n", primes[4]); // only test for me
+    // printf("%d\n", primes[3]); // only test for me
 }
 
-void decompose(int n)
+void decompose(int n, int primes[])
 {
-    // int actual_num = n;
-    // for (int i = 0; i < primes_in_mil; ++i) {
-    //     if (actual_num % i = 0) {
-    //         actual_num = actual_num / i;
-    //     }
-    // }
-    // printf("%d\n", n);
+    int counter = 0;
+    int actual_num = n;
+    for (int i = 0; i < primes_in_mil; ++i) {
+        counter = 0;
+        while (actual_num % primes[i] == 0) {
+            actual_num = actual_num / primes[i];
+            counter += 1;
+        }
+        if (n == 1) {
+            printf("1\n");
+            return;
+        }
+        if (counter > 1)
+            printf("%d^%d", primes[i], counter);
+        if (counter == 1)
+            printf("%d", primes[i]);
+        if (actual_num == 1) {
+            printf("\n");
+            return;
+        }
+        if (counter != 0)
+            printf(" x ");
+    }
 }
