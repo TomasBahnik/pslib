280b137133825236da392082c4ff86d01e8d7e22
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-03-14 21:39:18 +0100

    add 2 simple players

---
+++ python/KUI/reversi/random_player.py
@@ -0,0 +1,12 @@
+import random
+
+import player
+
+
+class MyPlayer(player.MyPlayer):
+    def __init__(self, my_color, opponent_color, board_size=8):
+        super().__init__(my_color, opponent_color, board_size)
+
+    def move(self, board):
+        moves = self.get_all_valid_moves(board)
+        return random.choice(moves) if len(moves) > 0 else None
