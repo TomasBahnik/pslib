3c4a3d6e082677382761cfcf390455e0d9908812
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-03-16 23:31:38 +0100

    random_player as game

--- python/KUI/reversi/random_player.py
+++ python/KUI/reversi/random_player.py
@@ -1,10 +1,12 @@
 import random
+from collections import namedtuple
 
-import math
+import numpy as np
 
 import player
 
-infinity = math.inf
+infinity = np.inf
+GameState = namedtuple('GameState', 'utility, board, moves')
 
 
 # TODO  must use only game board
@@ -52,7 +54,40 @@ def minimax_search(game, state):
 class MyPlayer(player.MyPlayer):
     def __init__(self, my_color, opponent_color, board_size=8):
         super().__init__(my_color, opponent_color, board_size)
+        self.name = 'RANDOM'
+        self.state = GameState(utility=0, board={}, moves=[])
 
+    # the only function with access to board
     def move(self, board):
         moves = self.get_all_valid_moves(board)
-        return random.choice(moves) if len(moves) > 0 else None
+        # main function to implement
+        move = random.choice(moves) if len(moves) > 0 else None
+        utility = self.compute_utility(board, move)
+        self.state = GameState(utility=utility, board=board, moves=moves)
+        return move
+
+    def actions(self):
+        """Legal moves are any square not yet taken."""
+        return self.state.moves
+
+    # TODO board is updated by game controller after move
+    def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        utility = self.compute_utility(board, move)
+        return GameState(utility=utility, board=board, moves=moves)
+
+    def utility(self, state, player_color):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player_color == self.my_color else -state.utility
+
+    def terminal_test(self, state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0
+
+    def compute_utility(self, board, move):
+        return len(self.state.moves)
