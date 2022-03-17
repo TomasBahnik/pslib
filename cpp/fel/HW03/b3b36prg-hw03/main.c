8d9c4ace72e6014cfc47539fbb05d39a5dc28847
pantom <pantom@centrum.cz>
2022-03-17 22:55:07 +0100

    add comments

--- cpp/fel/HW03/b3b36prg-hw03/main.c
+++ cpp/fel/HW03/b3b36prg-hw03/main.c
@@ -24,16 +24,17 @@ int main(int argc, char *argv[])
     str_enc = str = str_rot = NULL;
     str_enc_len = str_len = str_tmp_len = 0;
 
-    str_enc = read_input_message(&str_enc_len);
+    str_enc = read_input_message(&str_enc_len); // read first row
     if (str_enc) {
-        str = read_input_message(&str_len);
+        str = read_input_message(&str_len); // read second row
     }
     if (str_enc == NULL || str == NULL) {
-        ret = ERROR_INPUT;
+        ret = ERROR_INPUT; // exit if input is char from [a-z_A-Z]
     } else if (str_enc_len != str_len) {
-        ret = ERROR_LENGHT;
+        ret = ERROR_LENGHT; // exit if first and second row doesn't have same
+                            // lenght
     } else {
-        compare(str, str_enc, str_enc_len);
+        compare(str, str_enc, str_enc_len); // start comparing
     }
     print_error(ret);
     return ret;
@@ -46,6 +47,7 @@ char *read_input_message(int *str_len)
     char *str = malloc(capacity);
     if (str == NULL) {
         fprintf(stderr, " ERROR MALLOC\n"); // improbable
+        exit(-1);
     } else {
         int a;
         while ((a = getchar()) != EOF && a != '\n') {
@@ -78,23 +80,29 @@ char *read_input_message(int *str_len)
 
 void rotate(char original[], int len_org)
 {
+    // this function is rotating char array (first input row)
+    // it always rotate whole string plus one in ASCII table
+
     char c;
     for (int i = 0; i < len_org; ++i) {
         c = original[i];
         if (c == 0)
             break;
-        if (c == 'Z')
+        if (c == 'Z') // condition for end of big letters in alphabet
             c = 'a' - 1;
-        if (c == 'z')
+        if (c == 'z') // condition for end of small letters in alphabet
             c = 'A' - 1;
-        original[i] = c + 1;
+        original[i] = c + 1; // rotate array that you call with
     }
 }
 
 void compare(char str[], char str_enc[], int str_enc_len)
 {
+    // this function is comparing letter by letter of our rotated char array
+    // with second input row
+
     int counter = 0;
-    int match = 0;
+    int match = 0; // number of matches with input array
     char *best_match = malloc(str_enc_len * sizeof(char));
     char *str_tmp = malloc(str_enc_len * sizeof(char));
     for (int i = 0; i < str_enc_len; ++i) {
@@ -107,21 +115,21 @@ void compare(char str[], char str_enc[], int str_enc_len)
     for (int i = 0; i < 57; ++i) {
         for (int j = 0; j < str_enc_len; ++j) {
             if (str_tmp[j] == str[j])
-                counter++;
+                counter++; // counting letter matches with input array
         }
         if (match < counter) {
             match = counter;
             counter = 0;
-            memset(best_match, 0, str_enc_len);
+            memset(best_match, 0, str_enc_len); // everytime it find bigger
+                                                // match clear temporary array
             for (int k = 0; k < str_enc_len; ++k) {
-                best_match[k] = str_tmp[k];
-//                printf("%d\n",
-//                       str_tmp[10]); // <--- without this print doesnt work
+                best_match[k] =
+                    str_tmp[k]; // then write in word with more matches
             }
         } else {
             counter = 0;
         }
-        rotate(str_tmp, str_enc_len);
+        rotate(str_tmp, str_enc_len); // call rotate function
     }
     printf("%s\n", best_match);
     free(str_tmp);
@@ -138,5 +146,6 @@ void print_error(int error)
     case ERROR_LENGHT:
         fprintf(stderr, "%s\n", error_str_lenght);
         break;
+        // end switch
     }
 }
