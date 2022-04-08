51c31649c7ce3e33f3860925a70572dc254abead
pantom <pantom@centrum.cz>
2022-04-08 11:39:21 +0200

    TRY TESTS

--- cpp/fel/HW05/b3b36prg-hw05/main.c
+++ cpp/fel/HW05/b3b36prg-hw05/main.c
@@ -2,14 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_matrix(int rows, int cols, int[rows][cols]);
-bool sum(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2,
-         int matrix2[rows2][cols2], int rows3, int cols3,
-         int matrix3[rows3][cols3], int sign);
+int load_input(int r1, int c1, int r2, int c2);
 
+// work with matrix
+void print_matrix(int rows, int cols, int[rows][cols]);
 bool check_matrix_dim(int *r, int *c);
 bool read_matrix(int rows, int cols, int[rows][cols]);
 
+// operations
+bool sum(int r1, int c1, int m1[r1][c1], int r2, int c2, int m2[r2][c2], int r3,
+         int c3, int m3[r3][c3], int sign);
+bool multi(int r1, int c1, int m1[r1][c1], int r2, int c2, int m2[r2][c2],
+           int m3[r1][c2], int sign);
+
+// output value
 enum { ERROR_INPUT = 100 };
 
 int main(int argc, char *argv[])
@@ -31,7 +37,8 @@ int main(int argc, char *argv[])
     // loading char between matrix 1 and 2
     char op[2];
     if ((scanf("%1s", op)) != 1 ||
-        (op[0] != '+' || op[0] != '-' || op[0] != '*'))
+        (op[0] != '+' || op[0] != '-' ||
+         op[0] != '*')) //<--- TODO - if condiotin never happend
         ret = ERROR_INPUT;
     else
         ret = EXIT_SUCCESS;
@@ -52,30 +59,72 @@ int main(int argc, char *argv[])
     if (ret == ERROR_INPUT)
         fprintf(stderr, "Error: Chybny vstup!\n");
 
-    if (ret == EXIT_SUCCESS) {
-        print_matrix(row1, col1, m1);
-        printf("symbol: %s\n", op);
-        print_matrix(row2, col2, m2);
-    }
+    // if (ret == EXIT_SUCCESS) {
+    //     print_matrix(row1, col1, m1);
+    //     printf("symbol: %s\n", op);
+    //     print_matrix(row2, col2, m2);
+    // }
     int sign = 1;
+    int sum_res[row1][col1];
+    int mul_res[row1][col2];
     switch (op[0]) {
-    case '-':
+    case '-': // inversion to sum function
         sign = -1;
-    case '+': {
-        int result[row1][col1];
-        sum(row1, col1, m1, row2, col2, m2, row1, col1, result, sign);
-        print_matrix(row1, col1, result);
+    case '+': { // sum function
+        sum(row1, col1, m1, row2, col2, m2, row1, col1, sum_res, sign);
+        print_matrix(row1, col1, sum_res);
     } break;
-
+    case '*':
+        multi(row1, col1, m1, row2, col2, m2, mul_res, sign);
+        print_matrix(row1, col2, mul_res);
     default:
+        ret = ERROR_INPUT;
         break;
     }
+    return ret;
+}
+// TODO, only if its necesery (can be deleted)
+int load_input(int r1, int c1, int r2, int c2)
+{
+    int ret = 0;
+    // loading matrix 1
+    r1 = c1 = 1;
+    if (check_matrix_dim(&r1, &c1))
+        ret = EXIT_SUCCESS;
+    else
+        ret = ERROR_INPUT;
+    int m1[r1][c1];
+    if (ret == EXIT_SUCCESS && read_matrix(r1, c1, m1))
+        ret = EXIT_SUCCESS;
+    else
+        ret = ERROR_INPUT;
+
+    // loading char between matrix 1 and 2
+    char op[2];
+    if ((scanf("%1s", op)) != 1 ||
+        (op[0] != '+' || op[0] != '-' || op[0] != '*'))
+        ret = ERROR_INPUT;
+    else
+        ret = EXIT_SUCCESS;
+
+    // loading matrix 2
+    r2 = c2 = 1;
+    if (check_matrix_dim(&r2, &c2))
+        ret = EXIT_SUCCESS;
+    else
+        ret = ERROR_INPUT;
+    int m2[r2][c2];
+    if (ret == EXIT_SUCCESS && read_matrix(r2, c2, m2))
+        ret = EXIT_SUCCESS;
+    else
+        ret = ERROR_INPUT;
+
     return ret;
 }
 
 void print_matrix(int rows, int cols, int matrix[rows][cols])
 {
-    fprintf(stderr, "Dims: %d x %d\n", rows, cols);
+    fprintf(stderr, "%d %d\n", rows, cols);
     for (int r = 0; r < rows; ++r) {
         for (int c = 0; c < cols; ++c) {
             printf("%4d", matrix[r][c]);
@@ -109,14 +158,30 @@ bool read_matrix(int rows, int cols, int matrix[rows][cols])
     return ret;
 }
 
-bool sum(int rows1, int cols1, int matrix1[rows1][cols1], int rows2, int cols2,
-         int matrix2[rows2][cols2], int rows3, int cols3,
-         int matrix3[rows3][cols3], int sign)
+bool sum(int r1, int c1, int m1[r1][c1], int r2, int c2, int m2[r2][c2], int r3,
+         int c3, int m3[r3][c3], int sign)
 {
-    if (rows1 == rows2 && cols1 == cols2 && rows1 == rows3 && cols1 == cols3) {
-        for (int r = 0; r < rows1; ++r) {
-            for (int c = 0; c < cols1; ++c) {
-                matrix3[r][c] = matrix1[r][c] + sign * matrix2[r][c];
+    if (r1 == r2 && c1 == c2 && r1 == r3 && c1 == c3) {
+        for (int r = 0; r < r1; ++r) {
+            for (int c = 0; c < c1; ++c) {
+                m3[r][c] = m1[r][c] + sign * m2[r][c];
+            }
+        }
+        return true;
+    } else
+        return false;
+}
+
+bool multi(int r1, int c1, int m1[r1][c1], int r2, int c2, int m2[r2][c2],
+           int m3[r1][c2], int sign)
+{
+    if (c1 == r2) {
+        for (int r = 0; r < r1; ++r) {
+            for (int c = 0; c < c2; ++c) {
+                m3[r][c] = 0;
+                for (int i = 0; i < r2; ++i) {
+                    m3[r][c] += m1[r][i] * m2[i][c];
+                }
             }
         }
         return true;
