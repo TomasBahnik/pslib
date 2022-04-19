4421cb8a72bc9a0e565584047f23025646ca0151
pantom <pantom@centrum.cz>
2022-04-19 08:46:46 +0200

    only printing names of functions

---
+++ cpp/fel/HW06/b3b36prg-hw06/queue.c
@@ -0,0 +1,34 @@
+#include "queue.h"
+
+// TODO - your code
+queue_t *create_queue(int capacity)
+{
+    printf("create queue\n");
+    return 0;
+}
+
+void delete_queue(queue_t *queue) { printf("delete queue"); }
+
+bool push_to_queue(queue_t *queue, void *data)
+{
+    printf("push to queue\n");
+    return 0;
+}
+
+void *pop_from_queue(queue_t *queue)
+{
+    printf("pop from queue\n");
+    return 0;
+}
+
+void *get_from_queue(queue_t *queue, int idx)
+{
+    printf("get from queue\n");
+    return 0;
+}
+
+int get_queue_size(queue_t *queue)
+{
+    printf("get queue size\n");
+    return 0;
+}
