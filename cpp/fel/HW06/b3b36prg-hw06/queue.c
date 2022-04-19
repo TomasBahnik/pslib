53cc9d378bd4dacf6609412e93e7d34058b8a742
toba <toba@spoluzaci.cz>
2022-04-19 23:12:55 +0200

    circular queue with `void **data`

--- cpp/fel/HW06/b3b36prg-hw06/queue.c
+++ cpp/fel/HW06/b3b36prg-hw06/queue.c
@@ -1,34 +1,74 @@
 #include "queue.h"
 
-// TODO - your code
-queue_t *create_queue(int capacity)
+queue_t *create_queue(int max_size)
 {
-    printf("create queue\n");
-    return 0;
+    queue_t *temp = (queue_t *)malloc(sizeof(queue_t));
+    if (temp) {
+        temp->data = malloc(max_size * sizeof(void *));
+        if (temp->data) {
+            temp->head = 0;
+            temp->tail = 0;
+            temp->max_size = max_size;
+            temp->is_full = 0;
+        } else {
+            free(temp);
+            temp = NULL;
+        }
+    }
+    return temp;
 }
 
-void delete_queue(queue_t *queue) { printf("delete queue"); }
+void delete_queue(queue_t *queue)
+{
+    if (queue) {
+        free(queue->data);
+        free(queue);
+    }
+}
 
 bool push_to_queue(queue_t *queue, void *data)
 {
-    printf("push to queue\n");
-    return 0;
+    unsigned int result;
+    if (queue->is_full) {
+        cirque_resize(queue);
+        if (queue->is_full) {
+            result = 0;
+        }
+    }
+    if (!queue->is_full) {
+        queue->data[queue->tail++] = data;
+        if (queue->tail == queue->max_size) {
+            queue->tail = 0;
+        }
+        if (queue->tail == queue->head) {
+            queue->is_full = 1;
+        }
+        result = 1;
+    }
+    return result;
 }
 
 void *pop_from_queue(queue_t *queue)
 {
-    printf("pop from queue\n");
-    return 0;
+    void *data = NULL;
+    if (!cirque_is_empty(queue)) {
+        data = queue->data[queue->head];
+    }
+    return data;
 }
 
 void *get_from_queue(queue_t *queue, int idx)
 {
-    printf("get from queue\n");
-    return 0;
+    if (queue->head == queue->tail) {
+        printf("Queue Underflow!\n");
+        return NULL;
+    }
+    queue->head = (queue->head + 1) % queue->max_size;
+    return queue->data[idx];
 }
 
 int get_queue_size(queue_t *queue)
 {
     printf("get queue size\n");
-    return 0;
+    return queue->max_size;
 }
--- cpp/fel/HW06/b3b36prg-hw06/queue.h
+++ cpp/fel/HW06/b3b36prg-hw06/queue.h
@@ -7,10 +7,10 @@
 
 /* Queue structure which holds all necessary data */
 typedef struct {
-    // TODO - Include your data structure here
-    int head;
-    int tail;
-    int max_size;
+    unsigned int head;
+    unsigned int tail;
+    unsigned int is_full;
+    unsigned int max_size;
     void **data;
 } queue_t;
 
