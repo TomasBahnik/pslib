187080ef90ee59b0bb5fc01e9d6c06fbc0e4c873
pantom <pantom@centrum.cz>
2022-04-21 20:12:09 +0200

    push + pop with allocated array

--- cpp/fel/HW06/b3b36prg-hw06/queue.c
+++ cpp/fel/HW06/b3b36prg-hw06/queue.c
@@ -1,49 +1,59 @@
 #include "queue.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-enum { ERROR_OUTPUT = 101, OK = 1 };
+enum { QUEUE_EMPTY, INT_MIN };
 
-queue_t *create_queue(int capacity)
+// queue_t *create_queue(int capacity)
+// {
+//     queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
+//     queue->size = capacity;
+//     queue->array = malloc(sizeof(int) * queue->size);
+//     queue->num_entries = 0; // empty
+//     queue->head = 0;
+//     queue->tail = 0;
+//     return queue;
+// }
+
+void create_queue(queue_t *queue, int capacity)
 {
-    queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
     queue->size = capacity;
-    // queue->array = malloc(sizeof(int) * queue->size);
+    queue->array = malloc(sizeof(int) * 2 * queue->size);
     queue->num_entries = 0; // empty
     queue->head = 0;
     queue->tail = 0;
-    return queue;
 }
 
 void delete_queue(queue_t *queue)
 {
     free(queue->array);
-    printf("delete queue");
+    printf("delete queue\n");
 }
 
-bool push_to_queue(queue_t *queue, void *data)
+bool push_to_queue(queue_t *queue, int data)
 {
     if (is_full(queue)) {
-        fprintf(stderr, "queue is overflow !");
+        // fprintf(stderr, "queue is overflow !");
         return false;
-    } else {
-        queue->head = queue->tail = 0;
-        queue->array[queue->tail] = data;
-        queue->tail = (queue->tail + 1) % queue->size;
-        queue->num_entries++;
     }
+    // queue->head = queue->tail = 0;
+    queue->array[queue->tail] = data;
+    queue->num_entries++;
+    queue->tail = (queue->tail + 1) % queue->size;
 
     printf("push to queue\n");
-    printf("%d\n", data);
+    // printf("%d\n", data);
     return true;
 }
 
-void *pop_from_queue(queue_t *queue)
+int pop_from_queue(queue_t *queue)
 {
     int result;
 
     if (is_empty(queue)) {
-        fprintf(stderr, "queue is empty !");
-        // return 1;
+        // fprintf(stderr, "queue is empty !");
+        return QUEUE_EMPTY;
     }
     result = queue->array[queue->head];
     queue->head = (queue->head + 1) % queue->size;
@@ -65,13 +75,34 @@ int get_queue_size(queue_t *queue)
 }
 int is_full(queue_t *queue)
 {
-    if ((queue->tail + 1) % queue->size == queue->head)
-        return 1;
-    return 0;
+    return (queue->num_entries == queue->size);
+    // if ((queue->tail + 1) % queue->size == queue->head)
+    //     return 1;
+    // return 0;
 }
 int is_empty(queue_t *queue)
 {
-    if (queue->tail == -1 && queue->head == -1)
-        return 1;
-    return 0;
+    return (queue->num_entries == 0);
+    // if (queue->tail == -1 && queue->head == -1)
+    //     return 1;
+    // return 0;
+}
+
+int main()
+{
+    queue_t q1;
+
+    create_queue(&q1, 3); // first parametr is name of queue
+                          // second parametr is lenght of queue
+
+    push_to_queue(&q1, 58); // add 58
+    push_to_queue(&q1, 8);  // add 8
+    push_to_queue(&q1, 54); // add 54
+
+    int t;
+    while ((t = pop_from_queue(&q1)) != QUEUE_EMPTY) {
+        printf("t = %d\n", t); // poping elements while queue isn't empty
+    }
+
+    delete_queue(&q1); // free queue
 }
--- cpp/fel/HW06/b3b36prg-hw06/queue.h
+++ cpp/fel/HW06/b3b36prg-hw06/queue.h
@@ -15,7 +15,8 @@ typedef struct {
 } queue_t;
 
 /* creates a new queue with a given size */
-queue_t *create_queue(int capacity);
+// queue_t *create_queue(int capacity);
+void create_queue(queue_t *queue, int capacity);
 
 /* deletes the queue and all allocated memory */
 void delete_queue(queue_t *queue);
@@ -24,13 +25,13 @@ void delete_queue(queue_t *queue);
  * inserts a reference to the element into the queue
  * returns: true on success; false otherwise
  */
-bool push_to_queue(queue_t *queue, void *data);
+bool push_to_queue(queue_t *queue, int data);
 
 /*
  * gets the first element from the queue and removes it from the queue
  * returns: the first element on success; NULL otherwise
  */
-void *pop_from_queue(queue_t *queue);
+int pop_from_queue(queue_t *queue);
 
 /*
  * gets idx-th element from the queue, i.e., it returns the element that
