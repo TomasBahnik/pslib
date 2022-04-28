09650bdae5fe9f3be2b6a91622fdc90314c8cad9
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-04-28 20:47:34 +0200

    rename policy in sarsa

--- python/KUI/rl/rl_agent.py
+++ python/KUI/rl/rl_agent.py
@@ -103,7 +103,6 @@ def get_greedy_policy(q_table):
 
 def sarsa(env, num_episodes, eps0=0.5, alpha=0.5, max_trials=1000):
     """ On-policy Sarsa algorithm (with exploration rate decay) """
-
     # Env size
     x_dims = env.observation_space.spaces[0].n
     y_dims = env.observation_space.spaces[1].n
@@ -116,13 +115,13 @@ def sarsa(env, num_episodes, eps0=0.5, alpha=0.5, max_trials=1000):
     # check the sum of probabilities
     ones = np.ones([x_dims, y_dims], dtype=float)
     # Initialize policy to equal-probable random
-    policy = np.ones([x_dims, y_dims, n_action], dtype=float) / n_action
+    eps_greedy_policy = np.ones([x_dims, y_dims, n_action], dtype=float) / n_action
 
     for episode in range(num_episodes):
         # Reset the environment
         state = env.reset()
         state_idx = state[0:2]
-        action = np.random.choice(n_action, p=policy[state_idx[0], state_idx[1]])
+        action = np.random.choice(n_action, p=eps_greedy_policy[state_idx[0], state_idx[1]])
 
         done = False
         trials = 0
@@ -135,31 +134,34 @@ def sarsa(env, num_episodes, eps0=0.5, alpha=0.5, max_trials=1000):
             next_state_idx = next_state[0:2]
             next_state_idx_x = next_state_idx[0]
             next_state_idx_y = next_state_idx[1]
-            next_action = np.random.choice(n_action, p=policy[next_state_idx_x, next_state_idx_y])
+            # random choice from range(n_action) with probabilities p
+            next_action = np.random.choice(n_action, p=eps_greedy_policy[next_state_idx_x, next_state_idx_y])
 
-            # Update q values
+            # temporary difference update q values
             q[idx_x, idx_y, action] += alpha * (reward + q[next_state_idx_x, next_state_idx_y, next_action]
                                                 - q[idx_x, idx_y, action])
 
             # Extract eps-greedy policy from the updated q values
             eps = eps0 / (episode + 1)
             max_action_value = np.argmax(q[idx_x, idx_y])
-            policy[idx_x, idx_y, :] = eps / n_action
-            policy[idx_x, idx_y, max_action_value] = 1 - eps + eps / n_action
-            assert np.allclose(np.sum(policy, axis=2), ones)
+            eps_greedy_policy[idx_x, idx_y, :] = eps / n_action
+            eps_greedy_policy[idx_x, idx_y, max_action_value] = 1 - eps + eps / n_action
+            # check iif eps-greedy policy probabilities sum to ~1
+            assert np.allclose(np.sum(eps_greedy_policy, axis=2), ones)
 
             # Prepare the next q update
             state_idx = next_state_idx
             action = next_action
-    return q, policy
+    return q, eps_greedy_policy
 
 
 if __name__ == "__main__":
     # map_rel = 'maps_difficult/maze50x50.png'
     # map_rel = 'maps_difficult/maze50x50_22.png'
-    # map_rel = 'maps/easy/easy1.bmp'
+    # map_rel = 'maps/easy/easy2.bmp'
     # map_rel = 'maps/normal/normal12.bmp'
     map_rel = 'maps/normal/normal11.bmp'
+    # map_rel = 'maps/normal/normal10.bmp'
     MAP = os.path.join(os.path.dirname(os.path.abspath(__file__)), map_rel)
     # Initialize the maze environment
     env = kuimaze.HardMaze(map_image=MAP, probs=PROBS, grad=GRAD)
@@ -173,7 +175,6 @@ if __name__ == "__main__":
     if VERBOSITY > 0:
         # env.visualise(get_visualisation(q_table))
         greedy_policy = get_greedy_policy(q_table)
-        # print("policy {}".format(greedy_policy))
         env.visualise(get_visualisation_values(greedy_policy))
         env.render()
         sleep(20)
