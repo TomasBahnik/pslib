389525ecd465944c97f4fb5fc966762b5392e0e3
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-05-05 12:44:12 +0200

    class NB version

--- python/KUI/vr/classifier.py
+++ python/KUI/vr/classifier.py
@@ -57,10 +57,6 @@ def read_samples(truth_data):
         print(fpath)
 
 
-ASCII_CHARS_COUNT = 128
-GREY_COUNT = 256
-
-
 def samples(labeled_data: list, folder: str, img_size: int):
     n_samples = len(labeled_data)
     n_features = img_size
@@ -80,21 +76,68 @@ def samples(labeled_data: list, folder: str, img_size: int):
     return X, y
 
 
-# Multinomial NB estimates likelihoods
-def likelihoods(labeled_data: list, folder: str, img_size: int, grey_bits: int):
-    # Initialize likelihoods
-    l_h = np.zeros([ASCII_CHARS_COUNT, img_size, 2 ** grey_bits], dtype=int)
-    label_count = np.zeros([ASCII_CHARS_COUNT], dtype=int)
-    for l_d in labeled_data:
-        img_file = folder + '/' + l_d[0]
-        img_label_ascii = ord(l_d[1])
-        label_count[img_label_ascii] += 1
-        image = Image.open(img_file)
-        np_img = np.array(image).flatten()
-        for pixel in range(len(np_img)):
-            grey = np_img[pixel]
-            l_h[img_label_ascii][pixel][grey] += 1
-    return label_count, l_h
+class NaiveBayes:
+
+    def __init__(self, alpha=1.0, fit_prior=True):
+        # additive smoothing parameter
+        self.alpha = alpha
+        # prior class probability - uniform or empirical
+        self.fit_prior = fit_prior
+        self.classes = None
+        self.n_classes = None
+        self.n_samples = None
+        self.class_log_prior = None
+        self.feature_log_prob = None
+        self.log_class_count = None
+
+    def one_hot_enc(self, y):
+        classes = np.array(sorted(set(y)))
+        self.classes = classes
+        self.n_samples = len(y)
+        self.n_classes = len(classes)
+        y_in_classes = np.in1d(y, classes)
+        y_seen = y[y_in_classes]
+        sorted_class = np.sort(classes)
+        indices = np.searchsorted(sorted_class, y_seen)
+        indptr = np.hstack((0, np.cumsum(y_in_classes)))
+
+        data = np.empty_like(indices)
+        data.fill(1)
+        Y = sp.csr_matrix((data, indices, indptr), shape=(self.n_samples, self.n_classes))
+        Y = Y.toarray()
+        return Y
+
+    def fit(self, X, y):
+        Y = self.one_hot_enc(y)
+        n_classes = Y.shape[1]
+        if self.n_classes != n_classes:
+            print("Wrong one hot encoding")
+        n_features = X.shape[1]
+        # init count attributes
+        feature_count = np.zeros((n_classes, n_features), dtype=int)
+        class_count = np.zeros(n_classes, dtype=int)
+        # ret = np.dot(a, b) or ret = a @ b np.matmul(a,b)
+        feature_count += np.dot(Y.T, X)
+        class_count += Y.sum(axis=0)
+        # smoothed counts
+        smoothed_fc = feature_count + self.alpha
+        smoothed_cc = smoothed_fc.sum(axis=1)
+        # fit model
+        feature_log_prob = np.log(smoothed_fc) - np.log(smoothed_cc.reshape(-1, 1))
+        self.feature_log_prob = feature_log_prob
+        # TODO if/else uniform
+        # class_log_prior_ = np.full(n_classes, -np.log(n_classes))
+        # empirical prior
+        log_class_count = np.log(class_count)
+        self.log_class_count = log_class_count
+        class_log_prior = log_class_count - np.log(class_count.sum())
+        self.class_log_prior = class_log_prior
+
+    def predict(self, X):
+        """Calculate the posterior log probability of the samples X"""
+        log_post_prob = np.dot(X, self.feature_log_prob.T) + self.class_log_prior
+        predictions = self.classes[np.argmax(log_post_prob, axis=1)]
+        return predictions
 
 
 # TODO return
@@ -113,48 +156,12 @@ def n_b(img_dir, img_size):
     manual_n_b(X_test, X_train, y_test, y_train)
 
 
-def one_hot_enc(y):
-    classes = unique_labels(y)
-    n_samples = len(y)
-    n_classes = len(classes)
-    classes = np.asarray(classes)
-    y_in_classes = np.in1d(y, classes)
-    y_seen = y[y_in_classes]
-    sorted_class = np.sort(classes)
-    indices = np.searchsorted(sorted_class, y_seen)
-    indptr = np.hstack((0, np.cumsum(y_in_classes)))
-
-    data = np.empty_like(indices)
-    data.fill(1)
-    Y = sp.csr_matrix((data, indices, indptr), shape=(n_samples, n_classes))
-    Y = Y.toarray()
-    # Y = Y.astype(int, copy=False)
-    return Y, classes
-
-
-def manual_n_b(X_test, X_train, y_test, y_train, alpha=1.0):
+def manual_n_b(X_test, X_train, y_test, y_train):
     print("Using manual nb")
-    Y, classes_ = one_hot_enc(y_train)
-    n_classes = Y.shape[1]
-    _, n_features = X_train.shape
-    feature_count_ = np.zeros((n_classes, n_features), dtype=int)
-    class_count_ = np.zeros(n_classes, dtype=int)
-    # ret = np.dot(a, b) or ret = a @ b np.matmul(a,b)
-    feature_count_ += np.dot(Y.T, X_train)
-    class_count_ += Y.sum(axis=0)
-
-    smoothed_fc = feature_count_ + alpha
-    smoothed_cc = smoothed_fc.sum(axis=1)
-    feature_log_prob_ = np.log(smoothed_fc) - np.log(smoothed_cc.reshape(-1, 1))
-    # uniform
-    # class_log_prior_ = np.full(n_classes, -np.log(n_classes))
-    # empirical prior
-    log_class_count = np.log(class_count_)
-    class_log_prior_ = log_class_count - np.log(class_count_.sum())
-    """Calculate the posterior log probability of the samples X"""
-    jll = np.dot(X_test, feature_log_prob_.T) + class_log_prior_
-    predictions = classes_[np.argmax(jll, axis=1)]
-    c_m(predictions, y_test, classes_)
+    clf = NaiveBayes()
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+    c_m(predictions, y_test, clf.classes)
 
 
 def multinomial_n_b(X_test, X_train, y_test, y_train):
@@ -174,17 +181,6 @@ def c_m(predictions, y_test, classes, n_b_type=0):
     plt.show()
 
 
-def unique_labels(y):
-    ys = set(x for x in y)
-    return np.array(sorted(ys))
-
-
-def n_b_fit(X, y):
-    _, n_features = X.shape
-    Y = one_hot_enc(y)
-    return Y
-
-
 def main():
     parser = setup_arg_parser()
     args = parser.parse_args()
@@ -200,21 +196,9 @@ def main():
         # TODO Train and test the Naive Bayes classifier
 
 
-def test_1():
-    # shape (28,28,4)
-    img_dir = 'train_700_28'  # 28x28 = 784 32 bit
-    labeled = read_truth_dsv(img_dir, 'truth.dsv')
-    label_cnt, grey_cnt = likelihoods(labeled, img_dir, 28 * 28 * 4, 8)
-    for lbl in label_cnt.nonzero()[0]:
-        print("{}: count={}".format(chr(lbl), label_cnt[lbl]))
-        # nonzero [pixel,grey]
-        n_z_lbl_pix = grey_cnt[lbl].nonzero()
-        nz_greys = n_z_lbl_pix[0].nonzero()
-
-
 if __name__ == "__main__":
     # label_cnt, grey_cnt = likelihoods(labeled, img_dir, 10*10, 8)
     # n_b('train_1000_10', 10 * 10)  # 10x10 8 bit
-    n_b('train_1000_28', 28 * 28)  # 28x28 8 bit
-    # n_b('train_700_28', 28 * 28 * 4)  # 28x28 32 bit
+    # n_b('train_1000_28', 28 * 28)  # 28x28 8 bit
+    n_b('train_700_28', 28 * 28 * 4)  # 28x28 32 bit
     sys.exit(0)
