a4436944a87d56772328e703d5b3f9a4b0a4c259
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-05-05 19:46:10 +0200

    move scikit stuff to scikit_clf calculate image size in samples function n_b functions take only `args.train_path` use main for reading args

--- python/KUI/vr/classifier.py
+++ python/KUI/vr/classifier.py
@@ -2,13 +2,12 @@ import argparse
 import sys
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy.sparse as sp
 from PIL import Image
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+
+from scikit_clf import c_m
 
 
 def setup_arg_parser():
@@ -57,9 +56,12 @@ def read_samples(truth_data):
         print(fpath)
 
 
-def samples(labeled_data: list, folder: str, img_size: int):
+def samples(labeled_data: list, folder: str):
     n_samples = len(labeled_data)
-    n_features = img_size
+    img_file = folder + '/' + labeled_data[0][0]
+    image = Image.open(img_file)
+    np_img = np.array(image).flatten()
+    n_features = len(np_img)
     # sample values
     X = np.empty((n_samples, n_features), dtype=int)
     # target values = classes
@@ -143,16 +145,16 @@ class NaiveBayes:
 # TODO return
 # Výsledkem klasifikátoru je soubor classification.dsv stejného formátu jako truth.dsv,
 # který je umístěný v adresáři s testovacími obrázky.
-def n_b(img_dir, img_size):
+def n_b(img_dir):
     # TODO what about splitting to train and test labeled
     # so classification.dsv can be created from tests
     labeled = read_truth_dsv(img_dir, 'truth.dsv')
     # files = [x[0] for x in labeled]
     # targets = [x[1] for x in labeled]
     # files_train, files_test, targets_train, targets_test = train_test_split(files, targets)
-    X, y = samples(labeled, img_dir, img_size)
+    X, y = samples(labeled, img_dir)
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    multinomial_n_b(X_test, X_train, y_test, y_train)
+    # multinomial_n_b(X_test, X_train, y_test, y_train)
     manual_n_b(X_test, X_train, y_test, y_train)
 
 
@@ -162,23 +164,6 @@ def manual_n_b(X_test, X_train, y_test, y_train):
     clf.fit(X_train, y_train)
     predictions = clf.predict(X_test)
     c_m(predictions, y_test, clf.classes)
-
-
-def multinomial_n_b(X_test, X_train, y_test, y_train):
-    print("Using scikit nb")
-    clf = MultinomialNB()
-    clf.fit(X_train, y_train)
-    predictions = clf.predict(X_test)
-    c_m(predictions, y_test, clf.classes_, n_b_type=1)
-
-
-def c_m(predictions, y_test, classes, n_b_type=0):
-    cm = confusion_matrix(y_test, predictions, labels=classes)
-    disp_labels = [chr(x) for x in classes]
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=disp_labels)
-    color_bar = False if n_b_type == 0 else True
-    disp.plot(colorbar=color_bar)
-    plt.show()
 
 
 def main():
@@ -193,12 +178,9 @@ def main():
         # TODO Train and test the k-NN classifier
     elif args.b:
         print("Running Naive Bayes classifier")
-        # TODO Train and test the Naive Bayes classifier
+        n_b(args.train_path)  # 10x10 8 bit
 
 
 if __name__ == "__main__":
-    # label_cnt, grey_cnt = likelihoods(labeled, img_dir, 10*10, 8)
-    # n_b('train_1000_10', 10 * 10)  # 10x10 8 bit
-    # n_b('train_1000_28', 28 * 28)  # 28x28 8 bit
-    n_b('train_700_28', 28 * 28 * 4)  # 28x28 32 bit
+    main()
     sys.exit(0)
