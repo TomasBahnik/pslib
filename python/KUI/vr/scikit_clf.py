1fb72d0c607af51a5cb947381de24d47c61ef4e9
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-05-06 09:20:07 +0200

    add complement NB and sample test data

--- python/KUI/vr/scikit_clf.py
+++ python/KUI/vr/scikit_clf.py
@@ -1,11 +1,22 @@
+import sys
+
+import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, ComplementNB
 
 
 def multinomial_n_b(X_test, X_train, y_test, y_train):
-    print("Using scikit nb")
+    print("Using scikit multinomial nb")
     clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+    c_m(predictions, y_test, clf.classes_, n_b_type=1)
+
+
+def complement_n_b(X_test, X_train, y_test, y_train):
+    print("Using scikit complement nb")
+    clf = ComplementNB()
     clf.fit(X_train, y_train)
     predictions = clf.predict(X_test)
     c_m(predictions, y_test, clf.classes_, n_b_type=1)
@@ -18,3 +29,15 @@ def c_m(predictions, y_test, classes, n_b_type=0):
     color_bar = False if n_b_type == 0 else True
     disp.plot(colorbar=color_bar)
     plt.show()
+
+
+def sample_data():
+    rng = np.random.RandomState(1)
+    X = rng.randint(5, size=(7, 10))
+    y = np.array([1, 1, 2, 3, 4, 5, 6])
+    return X, y
+
+
+if __name__ == "__main__":
+    sample_data()
+    sys.exit(0)
