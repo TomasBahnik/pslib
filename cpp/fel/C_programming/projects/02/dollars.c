6bfa238dac95b93a87ffed3b5841ad28a0b23c3b
pantom <tomas.bahnik@gmail.com>
2022-08-05 11:39:22 +0200

    dollars.c some outputs are false

--- cpp/fel/C_programming/projects/02/dollars.c
+++ cpp/fel/C_programming/projects/02/dollars.c
@@ -3,27 +3,27 @@
 //
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int x;
     printf("Enter amount in $: ");
     scanf("%d", &x);
     int divisor = 20;
     int count = 0;
-    while (x > 0) {
-        if (x < 5){
+    while (x > 1) {
+        if (x < 5 && x > 1) {
             divisor = 1;
-        };
-        if (x % divisor == 0) {
-            count += 1;
+        }
+        if (x >= divisor) {
             x = x - divisor;
-            printf("$ %d bills: %d\n", divisor, count);
+            count += 1;
+            if (x < divisor || x == 1) {
+                printf("$ %d bills: %d\n", divisor, count);
+            }
         } else {
-            printf("$ %d bills: %d\n", divisor, count);
             count = 0;
             divisor = divisor / 2;
         }
-
-    };
-
+    }
     return 0;
 }
