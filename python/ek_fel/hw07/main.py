13fd9ea7e0bc697f17e4ef4650b2a08ef5952fed
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2022-11-10 17:46:48 +0100

    split processing to separate functions

--- python/ek_fel/hw07/main.py
+++ python/ek_fel/hw07/main.py
@@ -18,24 +18,41 @@ def load_input_file(file: Path) -> List[str]:
         return [line.strip() for line in input_file.readlines()]
 
 
-def list_operations(input_file: List[str]) -> List[str]:
-    ret = [line.strip() for line in input_file if line.strip() in OPERATIONS]
-    return ret
+def op_sign_idx(input_file: List[str]):
+    signs: List[str] = []
+    indexes: List[int] = []
+    for i in range(len(input_file)):
+        line = input_file[i].strip()
+        if line in OPERATIONS:
+            signs.append(line)
+            indexes.append(i)
+    return signs, indexes
 
 
 def matrix_data(input_file: List[str], op_indexes: List[int]):
-    m_data: List[List[str]] = []
+    """ reads matrix data from input file
+        first line are matrix dimensions rows x cols
+        the rest is matrix data
+    """
+    ret: List[List[str]] = []
     start_idx: int = 0
-    for o_i in op_indexes:
-        m_d = input_file[start_idx:o_i]
-        m_data.append(m_d)
-        start_idx = o_i + 1
+    for operation_idx in op_indexes:
+        # reads all lines related to matrix i.e.
+        # 1. row, cols
+        # 2 matrix data  = all lines up to next operation
+        m_d: List[str] = input_file[start_idx:operation_idx]
+        ret.append(m_d)
+        # move to index just after next operation
+        # i.e.  start of the next matrix
+        start_idx = operation_idx + 1
     last_m_data = input_file[start_idx:]
-    m_data.append(last_m_data)
-    return m_data
+    ret.append(last_m_data)
+    validate_matrix_data(ret)
+    return ret
 
 
 def validate_matrix_data(matrix_data: List[List[str]]):
+    """ check the dimensions and actual number of rows and cols"""
     for m_d in matrix_data:
         m_dims = list(map(int, m_d[0].split()))
         assert len(m_dims) == 2
@@ -49,93 +66,103 @@ def validate_matrix_data(matrix_data: List[List[str]]):
 
 
 def np_matrix_data(matrix_data: List[List[str]]) -> List[np.ndarray]:
+    """ convert string (numerical) matrices to np arrays of type int
+        easy to multiply and sum
+    """
     ret: List[np.ndarray] = []
     for m_d in matrix_data:
+        # skip the row with dimensions
         m_rows = m_d[1:]
         m_array = [list(map(int, row.split())) for row in m_rows]
         np_array = np.array(m_array, dtype=int)
-        # print(f"matrix array shape:{np_array.shape}, matrix dims : {m_d[0]}")
         ret.append(np.array(np_array, dtype=int))
     return ret
 
 
-def operations_idx(input_file: List[str], operations: List[str]) -> List[int]:
-    op_indexes: List[int] = []
-    start_idx: int = 0
-    # no Value error - all operations are in input file
-    for op in operations:
-        start_idx = input_file.index(op, start_idx)
-        op_indexes.append(start_idx)
-    return op_indexes
+def process_equal_priorities(operation_sign_priority, data: List[np.ndarray]):
+    """ there are only equal priorities evaluate expression from left to right
+        It is state when all higher priory expression were resolved
+        should be used for simple cases with just + and -
+    """
+    n_operations = len(operation_sign_priority)
+    np_r = data[0]
+    for i in range(n_operations):
+        current_op_sign: str = operation_sign_priority[i][0]
+        if current_op_sign == MULTIPLY:
+            np_r = np.matmul(data[i], data[i + 1])
+        if current_op_sign == SUBTRACT:
+            np_r = np_r - data[i + 1]
+        if current_op_sign == ADD:
+            np_r = np_r + data[i + 1]
+    remaining_ops = []
+    final_result = [np_r]
+    return remaining_ops, final_result
+
+
+def process_lower_priority(op_idx, new_ops, new_data, current_op_sign, same_priority, data):
+    """ if the next operation priority =  current_op_priority  add also next data = i + 1'
+        e.g. m1 + m2 -  means we can safely add next expression whatever it is
+        but. m1 + m2 *  means we have to evaluate next expression first!!
+    """
+    new_ops.append(current_op_sign)
+    if same_priority:
+        new_data.append(data[op_idx + 1])
+
+
+def process_higher_priority(op_idx, new_data, current_op_sign, data):
+    """ not the last operation
+        BUT current operation priority >= max_remaining_priorities
+    """
+    if current_op_sign == MULTIPLY:
+        tmp_result = np.matmul(data[op_idx], data[op_idx + 1])
+        new_data.append(tmp_result)
+    if current_op_sign == SUBTRACT:
+        tmp_result = data[op_idx] - data[op_idx + 1]
+        new_data.append(tmp_result)
+    if current_op_sign == ADD:
+        tmp_result = data[op_idx] + data[op_idx + 1]
+        new_data.append(tmp_result)
+
+
+def process_last_operation(op_idx, new_ops, new_data, current_op_sign, data):
+    new_ops.append(current_op_sign)
+    new_data.append(data[op_idx + 1])
 
 
 def process_ops(operation_sign_priority, data: List[np.ndarray]):
-    stop = len(operation_sign_priority)
+    n_operations = len(operation_sign_priority)
     new_ops = []
     new_data = []
-    current_priorities = set([x[1] for x in operation_sign_priority])
-    if len(current_priorities) == 1:
-        # all priorities are the same we can proceed from left to right
-        np_r = data[0]
-        for i in range(stop):
-            current_op_sign: str = operation_sign_priority[i][0]
-            # if current_op_sign == MULTIPLY:
-            #     np_r = np.matmul(data[i], data[i + 1])
-            if current_op_sign == SUBTRACT:
-                np_r = np_r - data[i + 1]
-            if current_op_sign == ADD:
-                np_r = np_r + data[i + 1]
-        return [], [np_r]
-
-    for i in range(stop):
+    current_priorities = [p[1] for p in operation_sign_priority]
+    # set keeps only unique elements
+    # i.e. if all are the same just one remains
+    if len(set(current_priorities)) == 1:
+        return process_equal_priorities(operation_sign_priority, data=data)
+    for i in range(n_operations):
+        not_last_operation = i < n_operations - 1
         current_op_priority: int = operation_sign_priority[i][1]
-        next_op_priority: int = operation_sign_priority[i + 1][1] if i < stop - 1 else None
+        next_op_priority: int = operation_sign_priority[i + 1][1] if not_last_operation else None
         same_priority: bool = current_op_priority == next_op_priority
         current_op_sign: str = operation_sign_priority[i][0]
-        current_priorities = ([x[1] for x in operation_sign_priority])
         max_remaining_priorities = max([x[1] for x in operation_sign_priority[i:]])
         if current_op_priority < max_remaining_priorities:
-            # if the next operation priority =  current_op_priority  add also f'm{i + 1}'
-            res = [current_op_sign, i + 1] if same_priority else [current_op_sign]
-            new_ops.append(current_op_sign)
-            if same_priority:
-                new_data.append(data[i + 1])
-                print(f"save({res} shape {data[i + 1].shape}")
-            else:
-                print(f"save({res} no data appended")
-        elif i < stop - 1:  # not last operation and curr_o_p >= max_remaining_priorities
-            res = [i, current_op_sign, i + 1]
-            np_r = None
-            if current_op_sign == MULTIPLY:
-                np_r = np.matmul(data[i], data[i + 1])
-                print(f"{i} {MULTIPLY} {i + 1} shape {np_r.shape}")
-                new_data.append(np_r)
-            if current_op_sign == SUBTRACT:
-                np_r = data[i] - data[i + 1]
-                print(f"{i} {SUBTRACT} {i + 1} shape {np_r.shape}")
-                new_data.append(np_r)
-            if current_op_sign == ADD:
-                np_r = data[i] + data[i + 1]
-                print(f"{i} {ADD} {i + 1} shape {np_r.shape}")
-                new_data.append(np_r)
-            else:
-                print(f"save({res})")
-        else:  # current = last operation
-            res = [current_op_sign, i + 1]
-            new_ops.append(current_op_sign)
-            new_data.append(data[i + 1])
-            print(f"save({res}) shape {data[i + 1].shape}")
+            process_lower_priority(op_idx=i, new_ops=new_ops, new_data=new_data,
+                                   current_op_sign=current_op_sign, same_priority=same_priority, data=data)
+        elif not_last_operation:
+            process_higher_priority(op_idx=i, new_data=new_data,
+                                    current_op_sign=current_op_sign, data=data)
+        else:
+            process_last_operation(op_idx=i, new_ops=new_ops, new_data=new_data,
+                                   current_op_sign=current_op_sign, data=data)
     return new_ops, new_data
 
 
 if __name__ == '__main__':
     f = Path(sys.argv[1])
     i_f = load_input_file(f)
-    ops = list_operations(i_f)
-    ops_idx = operations_idx(input_file=i_f, operations=ops)
+    ops, ops_idx = op_sign_idx(input_file=i_f)
     m_data = matrix_data(input_file=i_f, op_indexes=ops_idx)
     assert len(ops_idx) == len(m_data) - 1
-    validate_matrix_data(matrix_data=m_data)
     np_m_d: List[np.ndarray] = np_matrix_data(matrix_data=m_data)
     expected_result = np.matmul(np_m_d[0], np_m_d[1]) - np_m_d[2] + np.matmul(np_m_d[3], np_m_d[4]) + np_m_d[5]
     ops_prior = [OP_SIGN_PRIOR for o in ops if OP_SIGN_PRIOR[0] == o]
@@ -147,6 +174,6 @@ if __name__ == '__main__':
     print(f"2nd paas: {ops}")
     if len(np_m_d) == 1:
         result = np_m_d[0]
-        print(f"result: {result}")
-        print(f"expected_result : {expected_result}")
-        print(f"expected_result == result  : {expected_result == result}")
+        print(f"result\n{result}")
+        print(f"expected_result\n{expected_result}")
+        print(f"expected_result == result\n{expected_result == result}")
