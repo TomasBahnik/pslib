d3a5bb1b3e84116b82063f5c1981b651c3757532
Tomas Bahnik <tomas.bahnik@email.cz>
2024-03-06 19:49:32 +0100

    more complicated singletons

--- python/alp/s1/singleton.py
+++ python/alp/s1/singleton.py
@@ -3,15 +3,38 @@ from threading import Lock
 
 class SingletonNew:
     _instance = None
+    y = 4444
 
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if not cls._instance:
             with Lock():
+                # double-checked locking
                 if not cls._instance:
                     # in python 3.x super() add default arguments
                     # cls._instance = super(Singleton, cls).__new__(cls)
+                    # cannot call cls() - infinite recursion
+                    # object.__new__() takes exactly one argument (the type to instantiate)
                     cls._instance = super().__new__(cls)
         return cls._instance
+
+    # TypeError: SingletonNew.__new__() takes 1 positional argument but 2 were given
+    def __init__(self, x):
+        self.x = x
+
+
+class Singleton3(SingletonNew):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with Lock():
+                if not cls._instance:
+                    cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self, z):
+        super().__init__(z)
+        self.z = z
 
 
 class SingletonConstr:
--- python/alp/s1/singleton_test.py
+++ python/alp/s1/singleton_test.py
@@ -1,15 +1,22 @@
-from singleton import SingletonNew, SingletonConstr
+from singleton import SingletonNew, SingletonConstr, Singleton3
 
 
 def test_singleton():
-    s1 = SingletonNew()
-    s2 = SingletonNew()
+    s1 = SingletonNew(1)
+    s2 = SingletonNew(2)
     assert s1 is s2
     assert id(s1) == id(s2)
     assert s1 == s2
+    assert s1.x == s2.x
+    print(s1.x, s2.x)
 
     s3 = SingletonConstr.get_instance()
     s4 = SingletonConstr.get_instance()
     assert s3 is s4
     assert id(s3) == id(s4)
     assert s3 == s4
+
+    s4 = Singleton3(111)
+    assert s4.y == 4444
+    assert s4.x == 111
+    assert s4.z == 111
